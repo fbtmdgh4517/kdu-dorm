8699bd5e98784a7c6cb5c68eb2494e076ba0cc40
seungho <fbtmdgh4517@naver.com>
2023-03-28 23:19:24 +0900

    오늘의 기숙사 식단표를 보여주는 기능 추가

--- frontend/src/pages/WeekDiet.js
+++ frontend/src/pages/WeekDiet.js
@@ -3,18 +3,24 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const WeekDiet = () => {
-    const [mondayMeal, setMondayMeal] = useState([]);
+    const [todayDate, setTodayDate] = useState();
+    const [todayBreakfast, setTodayBreakfast] = useState();
+    const [todayDinner, setTodayDinner] = useState();
+
+    const fetchData = async () => {
+        try {
+            const res = await axios.get('http://localhost:4000/weekDiet', {
+                headers: {},
+            });
+            setTodayDate(res.data[0].todayDate);
+            setTodayBreakfast(res.data[0].todayBreakfast);
+            setTodayDinner(res.data[0].todayDinner);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await axios.get('http://localhost:4000/weekDiet', {
-                    headers: {},
-                });
-                console.log(res.data);
-            } catch (e) {
-                console.log(e);
-            }
-        };
         fetchData();
     }, []);
 
@@ -26,7 +32,46 @@ const WeekDiet = () => {
             >
                 경동대학교 기숙사 외박신청
             </Link>
-            <div className="border border-blue-200 max-w-sm w-11/12 mx-auto rounded-xl shadow-md p-8 bg-white mb-4"></div>
+            <div className="border border-blue-200 max-w-sm w-11/12 mx-auto rounded-xl shadow-md p-8 bg-white mb-4">
+                {todayDate && todayBreakfast && todayDinner ? (
+                    <>
+                        <p className="text-xl font-medium">기숙사 식단</p>
+                        <p className="text-xl font-medium">{todayDate}</p>
+                        <br></br>
+                        <div className="flex justify-between">
+                            <div className="space-x-3">
+                                <p className="text-xl font-medium">아침</p>
+                                {todayBreakfast[0] !== '' ? (
+                                    todayBreakfast.map((meal) => (
+                                        <p className="text-xl font-medium" key={meal}>
+                                            {meal}
+                                        </p>
+                                    ))
+                                ) : (
+                                    <p className="text-xl font-medium">식단이 없습니다.</p>
+                                )}
+                            </div>
+                        </div>
+                        <br></br>
+                        <div className="flex justify-between">
+                            <div className="space-x-3">
+                                <p className="text-xl font-medium">저녁</p>
+                                {todayDinner[0] !== '' ? (
+                                    todayDinner.map((meal) => (
+                                        <p className="text-xl font-medium" key={meal}>
+                                            {meal}
+                                        </p>
+                                    ))
+                                ) : (
+                                    <p className="text-xl font-medium">식단이 없습니다.</p>
+                                )}
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <p className="text-xl font-medium">로딩중</p>
+                )}
+            </div>
         </>
     );
 };
