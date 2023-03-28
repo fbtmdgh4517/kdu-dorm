772b1be0f55681d15b77af2026773ffc289d8683
seungho <fbtmdgh4517@naver.com>
2023-03-28 23:15:00 +0900

    오늘 식단만 불러오도록 변경

--- backend/src/routes/weekDiet.js
+++ backend/src/routes/weekDiet.js
@@ -1,29 +1,25 @@
 const cheerio = require('cheerio');
 const express = require('express');
 const request = require('request');
-const FormData = require('form-data');
-const axios = require('axios');
+// const FormData = require('form-data');
+// const axios = require('axios');
 const router = express.Router();
-
-const dietList = {
-    date: '',
-    breakfast: '',
-    dinner: '',
-};
 
 router.get('/', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     const meals = [];
+    const date = new Date();
+    let day = date.getDay().toString();
 
     let options = {
         method: 'POST',
         url: 'https://www.kduniv.ac.kr/kor/CMS/DietMenuMgr/list.do',
         headers: {
-            Cookie: 'JSESSIONID=D3D68E51124F13D7A286F97E9BEA5696.worker1',
+            // Cookie: 'JSESSIONID=D3D68E51124F13D7A286F97E9BEA5696.worker1',
         },
         formData: {
             mCode: 'MN183',
-            searchDay: '2023-03-27',
+            // searchDay: '2023-03-28',
             searchDietCategory: '5',
         },
     };
@@ -31,20 +27,37 @@ router.get('/', (req, res) => {
         if (error) throw new Error(error);
         let $ = cheerio.load(response.body);
         try {
-            $('tbody tr').each(function (index, elem) {
-                const date = $(elem).find('th').text().trim();
-                const breakfast = $(elem).find('td ul.res-depth1').text().trim().split('\n');
-                const dinner = $(elem).find('td ul.res-depth2').text().trim().split('\n');
-                if (date === '' && breakfast === '' && dinner === '') {
-                    //반복문 탈출
-                    return false;
-                }
-                meals.push({ date, breakfast, dinner });
-            });
+            if (day === '0') {
+                day = '7';
+            }
+            const todayDate = $(`tbody tr:nth-of-type(${day}) th`)
+                .text()
+                .trim()
+                .replace('\n\t\t\t\t\t\t', '.')
+                .replace('\n\t\t\t\t\t\t', ' ');
+            const todayBreakfast = $(`tbody tr:nth-of-type(${day}) td:nth-of-type(1)`).text().trim().split('\n');
+            const todayDinner = $(`tbody tr:nth-of-type(${day}) td:nth-of-type(2)`).text().trim().split('\n');
+            meals.push({ todayDate, todayBreakfast, todayDinner });
+            // console.log(meals);
+            // $('tbody tr').each(function (index, elem) {
+            //     const date = $(elem)
+            //         .find('th')
+            //         .text()
+            //         .trim()
+            //         .replace('\n\t\t\t\t\t\t', '.')
+            //         .replace('\n\t\t\t\t\t\t', ' ');
+            //     const breakfast = $(elem).find('td:nth-of-type(1)').text().trim().split('\n');
+            //     const dinner = $(elem).find('td:nth-of-type(2)').text().trim().split('\n');
+            //     if (date === '' && breakfast === '' && dinner === '') {
+            //         //반복문 탈출
+            //         return false;
+            //     }
+            //     meals.push({ date, breakfast, dinner });
+            // });
         } catch (error) {
             console.error(error);
         }
-        console.log(meals);
+        // console.log(meals);
         res.send(meals);
     });
 });
