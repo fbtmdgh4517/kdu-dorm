1cfc09c732c7bc99c0a23f8031930f90fc96257b
seungho <fbtmdgh4517@naver.com>
2023-03-29 22:49:41 +0900

    기숙사 공지사항들 제목 출력, 제목 클릭시 해당 공지사항 페이지로 이동 기능 구현

--- frontend/src/pages/NoticePage.js
+++ frontend/src/pages/NoticePage.js
@@ -11,7 +11,7 @@ const NoticePage = () => {
                 headers: {},
             });
             //res.data.root[0].list 배열과 res.data.root[0].topList 배열을 합치고 배열의 regdate를 기준으로 내림차순 정렬
-            console.log(res.data.root[0].list.concat(res.data.root[0].topList).sort((a, b) => b.regdate - a.regdate));
+            // console.log(res.data.root[0].list.concat(res.data.root[0].topList).sort((a, b) => b.regdate - a.regdate));
 
             // console.log(res.data.root[0].list.concat(res.data.root[0].topList));
             setNotice(res.data.root[0].list.concat(res.data.root[0].topList).sort((a, b) => b.regdate - a.regdate));
@@ -32,7 +32,27 @@ const NoticePage = () => {
             >
                 경동대학교 기숙사 외박신청
             </Link>
-            <div className="border border-blue-200 max-w-sm w-11/12 mx-auto rounded-xl shadow-md p-8 bg-white mb-4"></div>
+            <div className="border border-blue-200 max-w-sm w-11/12 mx-auto rounded-xl shadow-md p-8 bg-white mb-4">
+                {
+                    //notice가 있을 때 각 notice 배열의 subject를 출력
+                    notice &&
+                        notice.map((su) => {
+                            return (
+                                //새창으로 링크를 여는 코드
+                                <div key={su.seq}>
+                                    <a
+                                        href={`https://metrodorm.kduniv.ac.kr/bbs/getBbsWriteView.kmc?seq=${su.seq}&bbs_locgbn=KY&bbs_id=notice`}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        {su.subject}
+                                    </a>
+                                    <br></br>
+                                </div>
+                            );
+                        })
+                }
+            </div>
         </>
     );
 };
