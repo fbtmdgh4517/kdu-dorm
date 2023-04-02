35a57026da9b534df69744f9bcdba16817ec7cbd
seungho <fbtmdgh4517@naver.com>
2023-04-02 19:19:12 +0900

    일요일 날짜 오류 수정

--- backend/src/routes/weekDiet.js
+++ backend/src/routes/weekDiet.js
@@ -8,8 +8,13 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     const meals = [];
-    const date = new Date();
-    let day = date.getDay().toString();
+    const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+    const today = new Date();
+    const year = today.getFullYear().toString();
+    const month = (today.getMonth() + 1).toString();
+    const date = today.getDate().toString();
+    const day = dayOfWeek[today.getDay()];
+    let dayIndex = today.getDay().toString();
 
     let options = {
         method: 'POST',
@@ -27,20 +32,31 @@ router.get('/', (req, res) => {
         if (error) throw new Error(error);
         let $ = cheerio.load(response.body);
         try {
-            if (day === '0') {
-                day = '7';
+            const todayDate = `${year}년 ${month}월 ${date}일 ${day}요일`;
+            // if (day === '0') {
+            //     day = '7';
+            // }
+            // const todayDate = $(`tbody tr:nth-of-type(5) th`)
+            //     .text()
+            //     .trim()
+            //     .replace('\n\t\t\t\t\t\t', '.')
+            //     .replace('\n\t\t\t\t\t\t', ' ');
+            if (day === '금' || day === '토' || day === '일') {
+                const todayBreakfast = [''];
+                const todayDinner = [''];
+                meals.push({ todayDate, todayBreakfast, todayDinner });
+                console.log(meals);
+            } else {
+                const todayBreakfast = $(`tbody tr:nth-of-type(${dayIndex}) td:nth-of-type(1)`)
+                    .text()
+                    .trim()
+                    .split('\n');
+                const todayDinner = $(`tbody tr:nth-of-type(${dayIndex}) td:nth-of-type(2)`).text().trim().split('\n');
+                meals.push({ todayDate, todayBreakfast, todayDinner });
+                console.log(meals);
             }
-            const todayDate = $(`tbody tr:nth-of-type(${day}) th`)
-                .text()
-                .trim()
-                .replace('\n\t\t\t\t\t\t', '.')
-                .replace('\n\t\t\t\t\t\t', ' ');
-            const todayBreakfast = $(`tbody tr:nth-of-type(${day}) td:nth-of-type(1)`).text().trim().split('\n');
-            const todayDinner = $(`tbody tr:nth-of-type(${day}) td:nth-of-type(2)`).text().trim().split('\n');
-            meals.push({ todayDate, todayBreakfast, todayDinner });
-            // console.log(meals);
             // $('tbody tr').each(function (index, elem) {
-            //     const date = $(elem)
+            //     const today = $(elem)
             //         .find('th')
             //         .text()
             //         .trim()
@@ -48,11 +64,11 @@ router.get('/', (req, res) => {
             //         .replace('\n\t\t\t\t\t\t', ' ');
             //     const breakfast = $(elem).find('td:nth-of-type(1)').text().trim().split('\n');
             //     const dinner = $(elem).find('td:nth-of-type(2)').text().trim().split('\n');
-            //     if (date === '' && breakfast === '' && dinner === '') {
+            //     if (today === '' && breakfast === '' && dinner === '') {
             //         //반복문 탈출
             //         return false;
             //     }
-            //     meals.push({ date, breakfast, dinner });
+            //     meals.push({ today, breakfast, dinner });
             // });
         } catch (error) {
             console.error(error);
