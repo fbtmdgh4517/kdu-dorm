1ed5b72a550f1fbd7b2edf47e75bcfee2d846d4b
seungho <fbtmdgh4517@naver.com>
2023-04-04 21:23:38 +0900

    생성한 모델들 4개를 시퀄라이즈에 연결

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
+const Student = require('./student');
+const Administrator = require('./administrator');
+const OvernightApplication = require('./overnightApplication');
+const ScoreRecord = require('./scoreRecord');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
@@ -6,5 +10,20 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
+
+db.Student = Student;
+db.Administrator = Administrator;
+db.OvernightApplication = OvernightApplication;
+db.ScoreRecord = ScoreRecord;
+
+Student.initiate(sequelize);
+Administrator.initiate(sequelize);
+OvernightApplication.initiate(sequelize);
+ScoreRecord.initiate(sequelize);
+
+Student.associate(db);
+Administrator.associate(db);
+OvernightApplication.associate(db);
+ScoreRecord.associate(db);
 
 module.exports = db;
