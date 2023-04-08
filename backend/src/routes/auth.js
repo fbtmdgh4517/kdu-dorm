8b2c76c2438b5765f6428cc1d3a3fa059339dc34
seungho <fbtmdgh4517@naver.com>
2023-04-08 23:54:48 +0900

    로그인, 로그아웃 과정 수정

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,13 +1,138 @@
 const express = require('express');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
 const { login, logout } = require('../controllers/auth');
+const db = require('../../db');
 
 const router = express.Router();
 
-router.post('/login', isNotLoggedIn, login);
+// router.post(
+//     '/login',
+//     (req, res, next) => {
+//         console.log(req.isAuthenticated() + 'isLoggedOut');
+//         if (!req.isAuthenticated()) {
+//             console.log('로그인 상태 아님');
+//             next();
+//         } else {
+//             console.log('로그인 상태');
+//             const message = encodeURIComponent('로그인한 상태입니다.');
+//             res.redirect(`/?error=${message}`);
+//         }
+//     },
+//     (req, res, next) => {
+//         passport.authenticate('local', async (authError, student, info) => {
+//             console.log(student);
+//             if (authError) {
+//                 console.error(authError);
+//                 return next(authError);
+//             }
+//             if (!student) {
+//                 console.log(info.reason);
+//                 return res.status(401).send('오류');
+//             }
+//             return req.login(student, (loginError) => {
+//                 if (loginError) {
+//                     console.error(loginError);
+//                     return next(loginError);
+//                 }
+//                 console.log('로그인 성공');
+//                 console.log(req.isAuthenticated());
+//                 console.log(req.session.is_logined);
+//                 if (req.isAuthenticated()) {
+//                     return res.send(student.student_name);
+//                 } else {
+//                     return res.status(401).send('로그인 실패');
+//                 }
+//                 // return res.send(student.student_name);
+//                 // return res.redirect('/');
+//             });
+//         })(req, res, next);
+//     }
+// );
 
-router.get('/logout', isLoggedIn, logout);
+router.post('/login', (req, res) => {
+    const studentId = req.body.student_id;
+    const studentPassword = req.body.student_password;
+    const sendData = { isLogin: '', studentName: '', sessionID: '' };
+
+    console.log(studentId);
+    console.log(studentPassword);
+
+    if (studentId && studentPassword) {
+        db.query('SELECT * FROM students where student_id = ?', [studentId], (error, results, fields) => {
+            if (error) throw error;
+            if (results.length > 0) {
+                bcrypt.compare(studentPassword, results[0].student_password, (err, result) => {
+                    if (result) {
+                        req.session.is_logined = true;
+                        req.session.student_id = studentId;
+                        req.session.save(() => {
+                            sendData.studentName = results[0].student_name;
+                            sendData.isLogin = 'True';
+                            sendData.sessionID = req.sessionID;
+                            res.send(sendData);
+                            console.log('로그인 성공');
+                        });
+                        console.log(req.sessionID);
+                    } else {
+                        sendData.isLogin = '로그인 정보 일치하지 않음';
+                        res.send(sendData);
+                    }
+                });
+            } else {
+                sendData.isLogin = '아이디 정보 일치하지 않음';
+                res.send(sendData);
+            }
+        });
+    } else {
+        sendData.isLogin = '아이디와 비밀번호를 입력해주세요';
+        res.send(sendData);
+    }
+});
+
+router.get('/authcheck', (req, res) => {
+    const sendData = { isLogin: '' };
+    console.log(req.sessionID);
+    if (req.session.is_logined) {
+        sendData.isLogin = 'True';
+    } else {
+        sendData.isLogin = 'False';
+    }
+    res.send(sendData);
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('session-cookie', { path: '/' });
+    req.session.destroy(() => {
+        console.log('로그아웃 성공');
+        res.redirect('/');
+    });
+});
+
+// router.get(
+//     '/logout',
+//     (req, res, next) => {
+//         console.log(req.isAuthenticated() + 'isLoggedIn');
+//         console.log(req.session.is_logined);
+//         // console.log(req);
+//         if (req.isAuthenticated()) {
+//             console.log('로그인 상태');
+//             next();
+//         } else {
+//             console.log('로그인 필요');
+//             res.status(403).send('로그인 필요');
+//         }
+//     },
+//     (req, res) => {
+//         console.log('로그아웃 성공');
+//         req.logout();
+//         req.session.destroy(() => {
+//             res.clearCookie('session-cookie', { path: '/' });
+//             res.send('로그아웃 성공');
+//         });
+//     }
+// );
 
 module.exports = router;
