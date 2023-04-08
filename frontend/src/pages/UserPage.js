e03d558d3db763f65ccc607bff97e0276f21844d
seungho <fbtmdgh4517@naver.com>
2023-04-08 23:56:19 +0900

    로그아웃 기능 추가

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,6 +1,25 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const UserPage = () => {
+// eslint-disable-next-line react/prop-types
+const UserPage = ({ userName, removeUserHandler }) => {
+    const [name, setName] = useState('');
+    const [isLoggedOut, setIsLoggedOut] = useState(false);
+    const navigate = useNavigate();
+
+    const fetchData = async () => {
+        await axios.get('http://localhost:4000/application/list');
+    };
+
+    useEffect(() => {});
+
+    const onLogout = async () => {
+        await axios.get('http://localhost:4000/auth/logout', { withCredentials: true }).then((res) => {});
+        removeUserHandler();
+        // navigate('/');
+    };
+
     return (
         <>
             <Link
@@ -9,7 +28,14 @@ const UserPage = () => {
             >
                 경동대학교 기숙사 외박신청
             </Link>
+            <button
+                onClick={onLogout}
+                className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex"
+            >
+                로그아웃
+            </button>
             <div className="border border-blue-200 container max-w-[360px] mx-auto rounded-xl shadow-md p-4 bg-white mb-4">
+                {userName}
                 <h1 className="font-semibold mb-4">외박 신청 목록</h1>
                 <div className="max-w-md container mx-auto mb-4">
                     <Link className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2 flex items-center">
@@ -26,9 +52,12 @@ const UserPage = () => {
                         몇월 몇일 외박 신청
                     </Link>
                 </div>
-                <button className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex">
+                <Link
+                    to="/sinchung"
+                    className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex"
+                >
                     외박신청
-                </button>
+                </Link>
             </div>
         </>
     );
