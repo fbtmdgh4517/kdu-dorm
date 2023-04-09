a6e518fffbb761e51fdf2eab6d2908f21dad232a
seungho <fbtmdgh4517@naver.com>
2023-04-09 23:03:55 +0900

    외박신청 목록 출력 기능 추가

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -2,17 +2,27 @@ import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-// eslint-disable-next-line react/prop-types
 const UserPage = ({ userName, removeUserHandler }) => {
     const [name, setName] = useState('');
     const [isLoggedOut, setIsLoggedOut] = useState(false);
     const navigate = useNavigate();
+    const [applicationInfo, setApplicationInfo] = useState([]);
 
     const fetchData = async () => {
-        await axios.get('http://localhost:4000/application/list');
+        await axios
+            .get('http://localhost:4000/application/list', { withCredentials: true })
+            .then((res) => {
+                console.log(res.data);
+                setApplicationInfo(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
 
-    useEffect(() => {});
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const onLogout = async () => {
         await axios.get('http://localhost:4000/auth/logout', { withCredentials: true }).then((res) => {});
@@ -37,21 +47,18 @@ const UserPage = ({ userName, removeUserHandler }) => {
             <div className="border border-blue-200 container max-w-[360px] mx-auto rounded-xl shadow-md p-4 bg-white mb-4">
                 {userName}
                 <h1 className="font-semibold mb-4">외박 신청 목록</h1>
-                <div className="max-w-md container mx-auto mb-4">
-                    <Link className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2 flex items-center">
-                        몇월 몇일 외박 신청
-                    </Link>
-                </div>
-                <div className="max-w-md container mx-auto mb-4">
-                    <Link className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2 flex items-center">
-                        몇월 몇일 외박 신청
-                    </Link>
-                </div>
-                <div className="max-w-md container mx-auto mb-4">
-                    <Link className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2 flex items-center">
-                        몇월 몇일 외박 신청
-                    </Link>
-                </div>
+                {applicationInfo.map((application) => {
+                    const applicationYear = new Date(application.application_time).getFullYear();
+                    const applicationMonth = new Date(application.application_time).getMonth() + 1;
+                    const applicationDate = new Date(application.application_time).getDate();
+                    return (
+                        <div key={application.application_id} className="max-w-md container mx-auto mb-4">
+                            <Link className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2 flex items-center">
+                                {`${applicationYear}년 ${applicationMonth}월 ${applicationDate}일 외박 신청`}
+                            </Link>
+                        </div>
+                    );
+                })}
                 <Link
                     to="/sinchung"
                     className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex"
