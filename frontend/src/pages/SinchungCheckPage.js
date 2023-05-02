60a98414bd5ea63610817033863d9da96334fd5a
seungho <fbtmdgh4517@naver.com>
2023-05-02 23:39:57 +0900

    외박신청 상세내용 추가

--- frontend/src/pages/SinchungCheckPage.js
+++ frontend/src/pages/SinchungCheckPage.js
@@ -1,61 +1,82 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import HeaderContainer from '../containers/HeaderContainer';
+import SidebarContainer from '../containers/SidebarContainer';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const SinchungCheckPage = () => {
+    //관리자가 체크하는 페이지
+    const { id } = useParams();
+    const [applicationInfo, setApplicationInfo] = useState();
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState();
+
+    const fetchData = async () => {
+        // console.log(id);
+        await axios
+            .get(`http://localhost:4000/application/${id}`, { withCredentials: true })
+            .then((res) => {
+                console.log(res.data[0]);
+                setApplicationInfo(res.data[0]);
+                const applicationStartYear = new Date(applicationInfo.start_date).getFullYear();
+                const applicationStartMonth = new Date(applicationInfo.start_date).getMonth() + 1;
+                const applicationStartDate = new Date(applicationInfo.start_date).getDate();
+                const applicationEndYear = new Date(applicationInfo.end_date).getFullYear();
+                const applicationEndMonth = new Date(applicationInfo.end_date).getMonth() + 1;
+                const applicationEndDate = new Date(applicationInfo.end_date).getDate();
+                setStartDate(`${applicationStartYear}-${applicationStartMonth}-${applicationStartDate}`);
+                setEndDate(`${applicationEndYear}-${applicationEndMonth}-${applicationEndDate}`);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
         <>
-            <Link
-                className="text-center my-20 w-[360px] mx-auto text-3xl font-bold flex items-center justify-center self-center"
-                to="/"
-            >
-                경동대학교 기숙사 외박신청
-            </Link>
-            <div className="border border-blue-200 container max-w-[360px] mx-auto rounded-xl shadow-md p-4 bg-white mb-4">
-                <h1 className="font-semibold mb-4">외박 신청</h1>
-                <form>
-                    <div className="max-w-md container mx-auto mb-4">
-                        <label htmlFor="room_num" className="font-medium">
-                            호실
-                        </label>
-                        <input
-                            name="room_num"
-                            id="room_num"
-                            className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2"
-                            value="6432"
-                        />
-                    </div>
-                    <div className="max-w-md container mx-auto mb-4">
-                        <label htmlFor="date" className="font-medium">
-                            외박 기간
-                        </label>
-                        <input
-                            name="date"
-                            id="date"
-                            className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2 text-gray-400"
-                            type="datetime-local"
-                        />
-                        ~
-                        <input
-                            name="date"
-                            id="date"
-                            className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 px-2 text-gray-400"
-                            type="datetime-local"
-                        />
-                    </div>
-                    <div className="max-w-md container mx-auto mb-4">
-                        <label htmlFor="reason" className="font-medium">
-                            외박 사유
-                        </label>
-                        <input
-                            name="reason"
-                            id="reason"
-                            className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-20 px-2 overflow-auto"
-                            value="aaaaaaaaaaaaaaaaaaaaaaaa"
-                        />
-                    </div>
-                    <button className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex">
-                        신청
-                    </button>
-                </form>
+            <HeaderContainer></HeaderContainer>
+            <div className="flex overflow-hidden bg-white pt-16">
+                <SidebarContainer></SidebarContainer>
+                <div id="main-content" className="h-full w-full bg-gray-100 relative overflow-y-auto lg:ml-64">
+                    <main>
+                        <div className="py-6 px-4">
+                            <div className="w-full grid grid-cols-1 xl:grid-cols-2 gap-4">
+                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                                    {/* 카드 */}
+                                    <div className="flex items-center justify-between mb-4">
+                                        <h1 className="text-xl font-bold leading-none text-gray-900">외박신청</h1>
+                                    </div>
+                                    <div className="container mx-auto mb-5">
+                                        <h1 className="font-medium">외박 시작일</h1>
+                                        <span>{startDate}</span>
+                                    </div>
+                                    <div className="container mx-auto mb-5">
+                                        <h1 className="font-medium">외박 종료일</h1>
+                                        <span>{endDate}</span>
+                                    </div>
+                                    <div className="container mx-auto mb-5">
+                                        <h1 className="font-medium">외박 사유</h1>
+                                        <span>{applicationInfo.application_reason}</span>
+                                    </div>
+                                    <div className="flex justify-between mx-40">
+                                        <button className="shadow-md h-[35px] w-[85px] bg-blue-500 justify-center self-center text-base font-medium text-white rounded-3xl inline-flex items-center p-2 hover:bg-blue-700 transition ease-in-out hover:scale-110">
+                                            수락
+                                        </button>
+                                        <button className="shadow-md h-[35px] w-[85px] bg-red-500 justify-center self-center text-base font-medium text-white rounded-3xl inline-flex items-center p-2 hover:bg-red-700 transition ease-in-out hover:scale-110">
+                                            거절
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </div>
             </div>
         </>
     );
