7d0bd08cb4d5f367da317bc85ad5189a8c23b225
seungho <fbtmdgh4517@naver.com>
2023-05-06 15:37:18 +0900

    사용자 인증 정보를 recoil을 이용하여 관리하도록 수정

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,45 +2,49 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import AdminPage from './AdminPage';
 import UserPage from './UserPage';
+import { useRecoilValue } from 'recoil';
+import userAuthInfoSelector from '../state';
 
 const { Link } = require('react-router-dom');
 
 const MainPage = () => {
     const [user, setUser] = useState('');
-    const [studentId, setStudentId] = useState('');
     const [isLogin, setIsLogin] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
     const [admin, setAdmin] = useState('');
+    const userAuthInfo = useRecoilValue(userAuthInfoSelector);
 
     useEffect(() => {
-        axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
-            console.log(res.data);
-            if (res.data.isLogin === 'True') {
-                if (res.data.isAdmin === true) {
-                    setAdmin(res.data.studentId);
-                    setIsLogin(true);
-                    setIsAdmin(true);
-                } else {
-                    setUser(res.data.studentName);
-                    setStudentId(res.data.studentId);
-                    setIsAdmin(false);
-                    setIsLogin(true);
-                }
-            }
-        });
-    }, [isLogin]);
-
-    console.log(isLogin);
-    console.log(isAdmin);
+        if (!userAuthInfo.isLogin) {
+            alert('로그인이 필요합니다.');
+            window.location.href = '/';
+        }
+        // axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
+        //     console.log(res.data);
+        //     if (res.data.isLogin === 'True') {
+        //         if (res.data.isAdmin === true) {
+        //             setAdmin(res.data.studentId);
+        //             setIsLogin(true);
+        //             setIsAdmin(true);
+        //         } else {
+        //             setUser(res.data.studentName);
+        //             setIsAdmin(false);
+        //             setIsLogin(true);
+        //         }
+        //     } else {
+        //         alert('로그인이 필요합니다.');
+        //         window.location.href = '/';
+        //     }
+        // });
+    }, [userAuthInfo.isLogin]);
 
     const removeUserHandler = () => {
-        // setUser('');
         setIsLogin(false);
     };
 
-    if (isLogin && isAdmin) {
+    if (userAuthInfo.isLogin === 'True' && userAuthInfo.isAdmin) {
         return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
-    } else if (isLogin && !isAdmin) {
+    } else if (userAuthInfo.isLogin === 'True' && !userAuthInfo.isAdmin) {
         return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
     }
 };
