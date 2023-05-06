289c3303707a94432400dd53661d2a3788c8b4c1
seungho <fbtmdgh4517@naver.com>
2023-05-06 15:37:48 +0900

    사용자 인증 정보를 recoil을 이용하여 관리하도록 수정

--- frontend/src/containers/HeaderContainer.js
+++ frontend/src/containers/HeaderContainer.js
@@ -1,33 +1,41 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import { useRecoilValue } from 'recoil';
+import userAuthInfoSelector from '../state';
 
 const HeaderContainer = () => {
     const [user, setUser] = useState('');
-    const [studentId, setStudentId] = useState('');
     const [isLogin, setIsLogin] = useState(false);
-    const [isAdmin, setIsAdmin] = useState(false);
     const [admin, setAdmin] = useState('');
+    const userAuthInfo = useRecoilValue(userAuthInfoSelector);
+
+    console.log('ㄹrecoil');
+    console.log(userAuthInfo);
 
     useEffect(() => {
-        axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
-            console.log(res.data);
-            if (res.data.isLogin === 'True') {
-                if (res.data.isAdmin === true) {
-                    setUser(res.data.studentId);
-                    setIsLogin(true);
-                    setIsAdmin(true);
-                } else {
-                    setUser(res.data.studentName);
-                    setStudentId(res.data.studentId);
-                    setIsAdmin(false);
-                    setIsLogin(true);
-                }
-            }
-        });
+        if (userAuthInfo.isLogin === 'False') {
+            alert('로그인이 필요합니다.');
+            window.location.href = '/';
+        }
+        // axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
+        //     // console.log(res.data);
+        //     if (res.data.isLogin === 'True') {
+        //         if (res.data.isAdmin === true) {
+        //             setUser(res.data.studentId);
+        //             setIsLogin(true);
+        //         } else {
+        //             setUser(res.data.studentName);
+        //             setIsLogin(true);
+        //         }
+        //     } else {
+        //         alert('로그인이 필요합니다.');
+        //         window.location.href = '/';
+        //     }
+        // });
     }, [isLogin]);
 
-    return <Header userName={user}></Header>;
+    return <Header userName={userAuthInfo.studentId}></Header>;
 };
 
 export default HeaderContainer;
