7343e190d1afdcc8defbda527857d493080627f9
seungho <fbtmdgh4517@naver.com>
2023-05-07 22:46:01 +0900

    사용자 정보를 가져오는 방식을 수정

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import AdminPage from './AdminPage';
 import UserPage from './UserPage';
-import { useRecoilValue } from 'recoil';
-import userAuthInfoSelector from '../state';
+import { useRecoilRefresher_UNSTABLE, useRecoilState } from 'recoil';
+import userAuthInfoState from '../state';
+import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import { userAuthInfoSelector } from '../state';
 
 const { Link } = require('react-router-dom');
 
@@ -12,13 +14,23 @@ const MainPage = () => {
     const [isLogin, setIsLogin] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
     const [admin, setAdmin] = useState('');
-    const userAuthInfo = useRecoilValue(userAuthInfoSelector);
+    // const [userAuthInfo, setUserAuthInfo] = useRecoilState(userAuthInfoState);
+    const userAuthInfo = useRecoilValueLoadable(userAuthInfoSelector);
+    // const userAuthInfo = useRecoilValue(userAuthInfoSelector);
+    const refreshUserAuthInfo = useRecoilRefresher_UNSTABLE(userAuthInfoSelector);
 
     useEffect(() => {
-        if (!userAuthInfo.isLogin) {
-            alert('로그인이 필요합니다.');
-            window.location.href = '/';
-        }
+        refreshUserAuthInfo();
+        console.log(userAuthInfo);
+        // console.log(userAuthInfo.data);
+        // if (userAuthInfo.data.isLogin === 'False') {
+        //     alert('로그인이 필요합니다.');
+        //     window.location.href = '/';
+        // }
+        // if (userAuthInfo.isLogin === 'False') {
+        //     alert('로그인이 필요합니다.');
+        //     window.location.href = '/';
+        // }
         // axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
         //     console.log(res.data);
         //     if (res.data.isLogin === 'True') {
@@ -36,17 +48,36 @@ const MainPage = () => {
         //         window.location.href = '/';
         //     }
         // });
-    }, [userAuthInfo.isLogin]);
+    }, []);
 
     const removeUserHandler = () => {
         setIsLogin(false);
     };
 
-    if (userAuthInfo.isLogin === 'True' && userAuthInfo.isAdmin) {
-        return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
-    } else if (userAuthInfo.isLogin === 'True' && !userAuthInfo.isAdmin) {
-        return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
+    switch (userAuthInfo.state) {
+        case 'loading':
+            return <div>loading...</div>;
+        case 'hasError':
+            return <div>error...</div>;
+        default:
+            console.log(userAuthInfo.contents);
+            if (userAuthInfo.contents.data.isLogin === 'True' && userAuthInfo.contents.data.isAdmin) {
+                return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
+            } else if (userAuthInfo.contents.data.isLogin === 'True' && !userAuthInfo.contents.data.isAdmin) {
+                return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
+            }
     }
+
+    // if (userAuthInfo.data.isLogin === 'True' && userAuthInfo.data.isAdmin) {
+    //     return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
+    // } else if (userAuthInfo.data.isLogin === 'True' && !userAuthInfo.data.isAdmin) {
+    //     return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
+    // }
+    // if (userAuthInfo.isLogin === 'True' && userAuthInfo.isAdmin === 'True') {
+    //     return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
+    // } else if (userAuthInfo.isLogin === 'False' && userAuthInfo.isAdmin === 'False') {
+    //     return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
+    // }
 };
 
 export default MainPage;
