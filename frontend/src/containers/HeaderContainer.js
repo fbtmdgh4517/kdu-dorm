25fdfa7604a7f529240d430ed19843f00e343042
seungho <fbtmdgh4517@naver.com>
2023-05-11 18:17:03 +0900

    잘못 사용된 코드 수정

--- frontend/src/containers/HeaderContainer.js
+++ frontend/src/containers/HeaderContainer.js
@@ -2,21 +2,28 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import { useRecoilValue } from 'recoil';
-import userAuthInfoSelector from '../state';
+import { userAuthInfoSelector } from '../state';
+import { userAuthInfoState } from '../state';
 
 const HeaderContainer = () => {
     const [user, setUser] = useState('');
     const [isLogin, setIsLogin] = useState(false);
     const [admin, setAdmin] = useState('');
     const userAuthInfo = useRecoilValue(userAuthInfoSelector);
+    // const userAuthInfo = useRecoilValue(userAuthInfoState);
 
     console.log('ㄹrecoil');
     console.log(userAuthInfo);
 
     useEffect(() => {
-        if (userAuthInfo.isLogin === 'False') {
+        if (userAuthInfo.data.isLogin === 'False') {
             alert('로그인이 필요합니다.');
             window.location.href = '/';
+        }
+        if (userAuthInfo.data.isAdmin) {
+            setUser(userAuthInfo.data.studentId);
+        } else if (!userAuthInfo.data.isAdmin) {
+            setUser(userAuthInfo.data.studentName);
         }
         // axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
         //     // console.log(res.data);
@@ -33,9 +40,9 @@ const HeaderContainer = () => {
         //         window.location.href = '/';
         //     }
         // });
-    }, [isLogin]);
+    }, []);
 
-    return <Header userName={userAuthInfo.studentId}></Header>;
+    return <Header userName={user}></Header>;
 };
 
 export default HeaderContainer;
