81143263992ff10ab2af35742464d7a4bda47ebd
seungho <fbtmdgh4517@naver.com>
2023-05-11 18:13:56 +0900

    불필요 코드 제거

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const passport = require('passport');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
 
-const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { login, logout } = require('../controllers/auth');
-const db = require('../../db');
+const db = require("../../db");
 
 const router = express.Router();
 
-router.post('/signupRequest', (req, res) => {
+router.post("/signupRequest", (req, res) => {
     const studentName = req.body.student_name;
     const studentId = req.body.student_id;
     const studentDepartment = req.body.student_department;
@@ -16,52 +14,52 @@ router.post('/signupRequest', (req, res) => {
     const studentRoom = req.body.student_room;
     const studentPassword = req.body.student_password;
 
-    const sendData = { isSuccess: '' };
+    const sendData = { isSuccess: "" };
 
     if (studentName && studentId && studentDepartment && studentContact && studentRoom && studentPassword) {
-        db.query('SELECT * FROM students where student_id = ?', [studentId], (error, results) => {
+        db.query("SELECT * FROM students where student_id = ?", [studentId], (error, results) => {
             if (error) throw error;
             if (results.length <= 0) {
                 const hashedPassword = bcrypt.hashSync(studentPassword, 10);
                 db.query(
-                    'INSERT INTO signup_requests (student_id, student_name, student_department, student_contact, student_room, student_password) VALUES (?, ?, ?, ?, ?, ?)',
+                    "INSERT INTO signup_requests (student_id, student_name, student_department, student_contact, student_room, student_password) VALUES (?, ?, ?, ?, ?, ?)",
                     [studentId, studentName, studentDepartment, studentContact, studentRoom, hashedPassword],
                     (error, results) => {
                         if (error) throw error;
-                        sendData.isSuccess = 'True';
+                        sendData.isSuccess = "True";
                         res.send(sendData);
                     }
                 );
             } else {
-                sendData.isSuccess = '이미 존재하는 아이디입니다.';
+                sendData.isSuccess = "이미 존재하는 아이디입니다.";
                 res.send(sendData);
             }
         });
     }
 });
 
-router.get('/signupRequest', (req, res) => {
-    db.query('SELECT * FROM signup_requests', (error, results) => {
+router.get("/signupRequest", (req, res) => {
+    db.query("SELECT * FROM signup_requests", (error, results) => {
         if (error) throw error;
         console.log(results);
         res.send(results);
     });
 });
 
-router.get('/signupRequest/:id', (req, res) => {
-    db.query('SELECT * FROM signup_requests WHERE request_id = ?', [req.params.id], (error, results) => {
+router.get("/signupRequest/:id", (req, res) => {
+    db.query("SELECT * FROM signup_requests WHERE request_id = ?", [req.params.id], (error, results) => {
         if (error) throw error;
         console.log(results);
         res.send(results);
     });
 });
 
-router.put('/signupRequest/accept/:id', (req, res) => {
-    console.log('회원가입 승인');
+router.put("/signupRequest/accept/:id", (req, res) => {
+    console.log("회원가입 승인");
     const id = req.params.id;
     const updateStatusQuery = 'UPDATE signup_requests SET request_status = "승인" WHERE request_id = ?';
     const insertStudentInfoQuery =
-        'INSERT INTO students (student_id, student_name, student_department, student_contact, student_room, student_password) SELECT student_id, student_name, student_department, student_contact, student_room, student_password FROM signup_requests WHERE request_id = ?';
+        "INSERT INTO students (student_id, student_name, student_department, student_contact, student_room, student_password) SELECT student_id, student_name, student_department, student_contact, student_room, student_password FROM signup_requests WHERE request_id = ?";
 
     db.query(updateStatusQuery, [id], (error, results) => {
         if (error) throw error;
@@ -72,8 +70,8 @@ router.put('/signupRequest/accept/:id', (req, res) => {
     });
 });
 
-router.put('/signupRequest/reject/:id', (req, res) => {
-    console.log('회원가입 거절');
+router.put("/signupRequest/reject/:id", (req, res) => {
+    console.log("회원가입 거절");
     const id = req.params.id;
     const rejectionReason = req.body.rejection_reason;
     const sqlQuery = 'UPDATE signup_requests SET request_status = "거절", rejection_reason = ? WHERE request_id = ?';
@@ -83,14 +81,19 @@ router.put('/signupRequest/reject/:id', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
     // 여기 바꿔야ㅐ됨
     const id = req.body.student_id;
     const studentPassword = req.body.student_password;
-    const sendData = { isLogin: '', studentName: '', sessionID: '', isAdmin: '' };
+    const sendData = {
+        isLogin: "",
+        studentName: "",
+        sessionID: "",
+        isAdmin: "",
+    };
 
     if (id && studentPassword) {
-        db.query('SELECT * FROM students where student_id = ?', [id], (error, results) => {
+        db.query("SELECT * FROM students where student_id = ?", [id], (error, results) => {
             if (error) throw error;
             if (results.length > 0) {
                 bcrypt.compare(studentPassword, results[0].student_password, (err, result) => {
@@ -101,21 +104,21 @@ router.post('/login', (req, res) => {
                         req.session.is_admin = false;
                         req.session.save(() => {
                             sendData.studentName = results[0].student_name;
-                            sendData.isLogin = 'True';
+                            sendData.isLogin = "True";
                             sendData.sessionID = req.sessionID;
-                            sendData.isAdmin = 'False';
+                            sendData.isAdmin = "False";
                             res.send(sendData);
-                            console.log('로그인 성공');
+                            console.log("로그인 성공");
                         });
                         console.log(req.sessionID);
                     } else {
-                        sendData.isLogin = '로그인 정보 일치하지 않음';
+                        sendData.isLogin = "로그인 정보 일치하지 않음";
                         res.send(sendData);
                     }
                 });
             } else if (results.length <= 0) {
-                console.log('관리자');
-                db.query('SELECT * FROM administrators where admin_id = ?', [id], (error, results) => {
+                console.log("관리자");
+                db.query("SELECT * FROM administrators where admin_id = ?", [id], (error, results) => {
                     console.log(results);
                     if (error) throw error;
                     if (results.length > 0) {
@@ -127,38 +130,43 @@ router.post('/login', (req, res) => {
                                 req.session.is_admin = true;
                                 req.session.save(() => {
                                     sendData.studentName = results[0].admin_id;
-                                    sendData.isLogin = 'True';
+                                    sendData.isLogin = "True";
                                     sendData.sessionID = req.sessionID;
-                                    sendData.isAdmin = 'True';
+                                    sendData.isAdmin = "True";
                                     res.send(sendData);
-                                    console.log('관리자 로그인 성공');
+                                    console.log("관리자 로그인 성공");
                                 });
                                 console.log(req.sessionID);
                             } else {
-                                sendData.isLogin = '로그인 정보 일치하지 않음';
+                                sendData.isLogin = "로그인 정보 일치하지 않음";
                                 res.send(sendData);
                             }
                         });
                     }
                 });
             } else {
-                sendData.isLogin = '아이디 정보 일치하지 않음';
+                sendData.isLogin = "아이디 정보 일치하지 않음";
                 res.send(sendData);
             }
         });
     } else {
-        sendData.isLogin = '아이디와 비밀번호를 입력해주세요';
+        sendData.isLogin = "아이디와 비밀번호를 입력해주세요";
         res.send(sendData);
     }
 });
 
-router.get('/authcheck', (req, res) => {
-    const sendData = { isLogin: '', studentId: '', studentName: '', isAdmin: '' };
-    console.log('authcheck');
+router.get("/authcheck", (req, res) => {
+    const sendData = {
+        isLogin: "",
+        studentId: "",
+        studentName: "",
+        isAdmin: "",
+    };
+    console.log("authcheck");
     console.log(req.sessionID);
     console.log(req.session);
     if (req.session.is_logined) {
-        sendData.isLogin = 'True';
+        sendData.isLogin = "True";
         if (req.session.is_admin === true) {
             sendData.studentId = req.session.admin_id;
             sendData.isAdmin = req.session.is_admin;
@@ -168,85 +176,17 @@ router.get('/authcheck', (req, res) => {
             sendData.isAdmin = req.session.is_admin;
         }
     } else {
-        sendData.isLogin = 'False';
+        sendData.isLogin = "False";
     }
     res.send(sendData);
 });
 
-router.get('/logout', (req, res) => {
-    res.clearCookie('session-cookie', { path: '/' });
+router.get("/logout", (req, res) => {
+    res.clearCookie("session-cookie", { path: "/" });
     req.session.destroy(() => {
-        console.log('로그아웃 성공');
-        res.redirect('/');
+        console.log("로그아웃 성공");
+        res.redirect("/");
     });
 });
-
-// router.post(
-//     '/login',
-//     (req, res, next) => {
-//         console.log(req.isAuthenticated() + 'isLoggedOut');
-//         if (!req.isAuthenticated()) {
-//             console.log('로그인 상태 아님');
-//             next();
-//         } else {
-//             console.log('로그인 상태');
-//             const message = encodeURIComponent('로그인한 상태입니다.');
-//             res.redirect(`/?error=${message}`);
-//         }
-//     },
-//     (req, res, next) => {
-//         passport.authenticate('local', async (authError, student, info) => {
-//             console.log(student);
-//             if (authError) {
-//                 console.error(authError);
-//                 return next(authError);
-//             }
-//             if (!student) {
-//                 console.log(info.reason);
-//                 return res.status(401).send('오류');
-//             }
-//             return req.login(student, (loginError) => {
-//                 if (loginError) {
-//                     console.error(loginError);
-//                     return next(loginError);
-//                 }
-//                 console.log('로그인 성공');
-//                 console.log(req.isAuthenticated());
-//                 console.log(req.session.is_logined);
-//                 if (req.isAuthenticated()) {
-//                     return res.send(student.student_name);
-//                 } else {
-//                     return res.status(401).send('로그인 실패');
-//                 }
-//                 // return res.send(student.student_name);
-//                 // return res.redirect('/');
-//             });
-//         })(req, res, next);
-//     }
-// );
-
-// router.get(
-//     '/logout',
-//     (req, res, next) => {
-//         console.log(req.isAuthenticated() + 'isLoggedIn');
-//         console.log(req.session.is_logined);
-//         // console.log(req);
-//         if (req.isAuthenticated()) {
-//             console.log('로그인 상태');
-//             next();
-//         } else {
-//             console.log('로그인 필요');
-//             res.status(403).send('로그인 필요');
-//         }
-//     },
-//     (req, res) => {
-//         console.log('로그아웃 성공');
-//         req.logout();
-//         req.session.destroy(() => {
-//             res.clearCookie('session-cookie', { path: '/' });
-//             res.send('로그아웃 성공');
-//         });
-//     }
-// );
 
 module.exports = router;
