7c46d3f1b76fd7e2495cd11a7357189c9ff8dafa
seungho <fbtmdgh4517@naver.com>
2023-05-12 19:43:37 +0900

    외박신청 삭제 기능 추가

--- frontend/src/pages/SinchungCheckPage.js
+++ frontend/src/pages/SinchungCheckPage.js
@@ -49,6 +49,10 @@ const SinchungCheckPage = () => {
         fetchData();
     }, []);
 
+    const toggleUpdateButtonClicked = () => {
+        setIsUpdateBtnClicked(!isUpdateBtnClicked);
+    };
+
     const onAcceptApplication = async () => {
         await axios
             .put(`http://localhost:4000/application/accept/${id}`, { withCredentials: true })
@@ -86,6 +90,18 @@ const SinchungCheckPage = () => {
             alert("외박 신청 수정이 완료되었습니다.");
             setIsUpdateBtnClicked(false);
             fetchData();
+        }
+    };
+
+    const onDeleteApplication = async () => {
+        if (confirm("정말 삭제하시겠습니까?") === true) {
+            const res = await axios.delete(`http://localhost:4000/application/delete/${id}`, { withCredentials: true });
+            if (res.status === 200) {
+                alert("외박 신청 삭제가 완료되었습니다.");
+                window.location.href = "/main";
+            }
+        } else {
+            return;
         }
     };
 
@@ -170,9 +186,7 @@ const SinchungCheckPage = () => {
                                                         수정
                                                     </button>
                                                     <button
-                                                        onClick={() => {
-                                                            setIsUpdateBtnClicked(!isUpdateBtnClicked);
-                                                        }}
+                                                        onClick={toggleUpdateButtonClicked}
                                                         className="shadow-md h-9 w-20 bg-red-500 justify-center self-center text-base font-medium text-white rounded-3xl inline-flex items-center p-2 hover:bg-red-700 transition ease-in-out hover:scale-110"
                                                     >
                                                         취소
@@ -197,14 +211,20 @@ const SinchungCheckPage = () => {
                                         </>
                                     )}
                                     {!userAuthInfo.data.isAdmin && !isUpdateBtnClicked && applicationInfo.approval_status === "미확인" && (
-                                        <button
-                                            onClick={() => {
-                                                setIsUpdateBtnClicked(!isUpdateBtnClicked);
-                                            }}
-                                            className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex"
-                                        >
-                                            수정
-                                        </button>
+                                        <div className="flex justify-between  md:mx-40 my-5">
+                                            <button
+                                                onClick={toggleUpdateButtonClicked}
+                                                className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex"
+                                            >
+                                                수정
+                                            </button>
+                                            <button
+                                                onClick={onDeleteApplication}
+                                                className="shadow-md rounded-3xl h-[35px] w-[85px] bg-red-500 items-center justify-center self-center text-base font-medium text-white hover:bg-red-700 mx-auto transition ease-in-out hover:scale-110 flex"
+                                            >
+                                                삭제
+                                            </button>
+                                        </div>
                                     )}
                                     {userAuthInfo.data.isAdmin && applicationInfo.approval_status === "미확인" && (
                                         <div className="flex justify-between  md:mx-40 my-5">
