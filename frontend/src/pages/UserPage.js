6981c80e82c063f83f7578f2b7409b21e7cbebd0
seungho <fbtmdgh4517@naver.com>
2023-05-13 23:03:46 +0900

    UI 수정

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -25,7 +25,7 @@ const UserPage = ({ userName, removeUserHandler }) => {
                 <div id="main-content" className="h-full w-full bg-gray-100 relative overflow-y-auto lg:ml-64">
                     <main>
                         <div className="py-6 px-4">
-                            <div className="w-full grid grid-cols-1 xl:grid-cols-2 gap-4">
+                            <div className="w-full grid grid-cols-1 xl:grid-cols-3 gap-4">
                                 <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
                                     {/* 카드 */}
                                     <UserApplicationList></UserApplicationList>
