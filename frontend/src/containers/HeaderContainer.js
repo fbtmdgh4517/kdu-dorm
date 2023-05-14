1bab392657ce7661323b301525dc37be447f8052
seungho <fbtmdgh4517@naver.com>
2023-05-14 21:32:01 +0900

    컴포넌트에 있던 로직들을 컨테이너로 이동

--- frontend/src/containers/HeaderContainer.js
+++ frontend/src/containers/HeaderContainer.js
@@ -1,48 +1,32 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Header from '../components/Header';
-import { useRecoilValue } from 'recoil';
-import { userAuthInfoSelector } from '../state';
-import { userAuthInfoState } from '../state';
+import { useEffect, useState } from "react";
+import Header from "../components/Header";
+import { useRecoilValue } from "recoil";
+import { userAuthInfoSelector } from "../state";
 
 const HeaderContainer = () => {
-    const [user, setUser] = useState('');
-    const [isLogin, setIsLogin] = useState(false);
-    const [admin, setAdmin] = useState('');
+    const [user, setUser] = useState("");
     const userAuthInfo = useRecoilValue(userAuthInfoSelector);
-    // const userAuthInfo = useRecoilValue(userAuthInfoState);
-
-    console.log('ㄹrecoil');
-    console.log(userAuthInfo);
 
     useEffect(() => {
-        if (userAuthInfo.data.isLogin === 'False') {
-            alert('로그인이 필요합니다.');
-            window.location.href = '/';
+        if (userAuthInfo.data.isLogin === "False") {
+            alert("로그인이 필요합니다.");
+            window.location.href = "/";
         }
         if (userAuthInfo.data.isAdmin) {
             setUser(userAuthInfo.data.studentId);
         } else if (!userAuthInfo.data.isAdmin) {
             setUser(userAuthInfo.data.studentName);
         }
-        // axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
-        //     // console.log(res.data);
-        //     if (res.data.isLogin === 'True') {
-        //         if (res.data.isAdmin === true) {
-        //             setUser(res.data.studentId);
-        //             setIsLogin(true);
-        //         } else {
-        //             setUser(res.data.studentName);
-        //             setIsLogin(true);
-        //         }
-        //     } else {
-        //         alert('로그인이 필요합니다.');
-        //         window.location.href = '/';
-        //     }
-        // });
     }, []);
 
-    return <Header userName={user}></Header>;
+    const onSideBarToggle = () => {
+        document.getElementById("sidebar").classList.toggle("hidden");
+        document.getElementById("toggleSidebarMobile").classList.toggle("bg-gray-100");
+        document.getElementById("toggleSidebarMobileHamburger").classList.toggle("hidden");
+        document.getElementById("toggleSidebarMobileClose").classList.toggle("hidden");
+    };
+
+    return <Header userName={user} onSideBarToggle={onSideBarToggle}></Header>;
 };
 
 export default HeaderContainer;
--- frontend/src/containers/SidebarContainer.js
+++ frontend/src/containers/SidebarContainer.js
@@ -1,19 +1,22 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import { useRecoilValue } from "recoil";
+import { userAuthInfoSelector } from "../state";
 
 const SidebarContainer = () => {
     const [isLogin, setIsLogin] = useState(false);
     const navigate = useNavigate();
+    const userAuthInfo = useRecoilValue(userAuthInfoSelector);
 
     const onLogout = async () => {
-        await axios.get('http://localhost:4000/auth/logout', { withCredentials: true }).then((res) => {});
+        await axios.get("http://localhost:4000/auth/logout", { withCredentials: true }).then((res) => {});
         setIsLogin(false);
-        navigate('/');
+        navigate("/");
     };
 
-    return <Sidebar onLogout={onLogout}></Sidebar>;
+    return <Sidebar onLogout={onLogout} userAuthInfo={userAuthInfo}></Sidebar>;
 };
 
 export default SidebarContainer;
