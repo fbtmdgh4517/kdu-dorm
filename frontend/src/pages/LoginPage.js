9a5212c37733d53f1f45c49ac5cd31180a3f15bc
seungho <fbtmdgh4517@naver.com>
2023-05-15 23:20:29 +0900

    불필요한 주석 제거

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,41 +1,22 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import UserPage from './UserPage';
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import UserPage from "./UserPage";
+import { useRecoilState } from "recoil";
+import userAuthInfoState from "../state";
 
 const LoginPage = (props) => {
     const { register, handleSubmit } = useForm();
     const navigate = useNavigate();
-    const [user, setUser] = useState('');
-    const [studentId, setStudentId] = useState('');
+    const [user, setUser] = useState("");
     const [isLogin, setIsLogin] = useState(false);
-    const [errMessage, setErrMessage] = useState('');
-    const [isAdmin, setIsAdmin] = useState(false);
-
-    // useEffect(() => {
-    //     axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
-    //         console.log(res.data);
-    //         if (res.data.isLogin === 'True') {
-    //             if (res.data.isAdmin === 'True') {
-    //                 setUser(res.data.studentName);
-    //                 setStudentId(res.data.studentId);
-    //                 setIsAdmin(true);
-    //                 setIsLogin(true);
-    //             } else {
-    //                 setUser(res.data.studentName);
-    //                 setStudentId(res.data.studentId);
-    //                 setIsAdmin(false);
-    //                 setIsLogin(true);
-    //             }
-    //         }
-    //     });
-    // }, [isLogin]);
+    const [errMessage, setErrMessage] = useState("");
 
     const onSubmit = (data) => {
         axios
             .post(
-                'http://localhost:4000/auth/login',
+                "http://localhost:4000/auth/login",
                 {
                     student_id: data.student_id,
                     student_password: data.student_password,
@@ -43,14 +24,14 @@ const LoginPage = (props) => {
                 { withCredentials: true }
             )
             .then((res) => {
-                if (res.data.isLogin !== 'True') {
+                if (res.data.isLogin !== "True") {
                     setErrMessage(res.data.isLogin);
                 }
                 console.log(res.data);
-                if (res.data.isLogin === 'True') {
+                if (res.data.isLogin === "True") {
                     setUser(res.data.studentName);
                     setIsLogin(true);
-                    navigate('/main');
+                    navigate("/main");
                 }
             })
             .catch((err) => {
@@ -58,19 +39,9 @@ const LoginPage = (props) => {
             });
     };
 
-    const removeUserHandler = () => {
-        // setUser('');
-        setIsLogin(false);
-    };
-
-    console.log(user);
-
     return (
         <>
-            <Link
-                className="text-center my-20 w-[360px] mx-auto text-3xl font-bold flex items-center justify-center self-center"
-                to="/"
-            >
+            <Link className="text-center my-20 w-[360px] mx-auto text-3xl font-bold flex items-center justify-center self-center" to="/">
                 경동대학교 기숙사 외박신청
             </Link>
             <div className="border border-blue-200 w-[360px] mx-auto rounded-xl shadow-md p-8 bg-white mb-4">
@@ -81,7 +52,7 @@ const LoginPage = (props) => {
                         </label>
                         <input
                             id="student_id"
-                            {...register('student_id')}
+                            {...register("student_id")}
                             className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 transition ease-in-out hover:scale-105 px-2"
                         />
                     </div>
@@ -91,7 +62,7 @@ const LoginPage = (props) => {
                         </label>
                         <input
                             id="student_password"
-                            {...register('student_password')}
+                            {...register("student_password")}
                             className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 transition ease-in-out hover:scale-105 px-2"
                             type="password"
                         />
@@ -111,56 +82,6 @@ const LoginPage = (props) => {
                     회원가입
                 </Link>
             </div>
-            {/* {!isLogin ? (
-                <>
-                    <Link
-                        className="text-center my-20 w-[360px] mx-auto text-3xl font-bold flex items-center justify-center self-center"
-                        to="/"
-                    >
-                        경동대학교 기숙사 외박신청
-                    </Link>
-                    <div className="border border-blue-200 w-[360px] mx-auto rounded-xl shadow-md p-8 bg-white mb-4">
-                        <form method="post" onSubmit={handleSubmit(onSubmit)}>
-                            <div className="max-w-md container mx-auto mb-8">
-                                <label htmlFor="student_id" className="font-medium">
-                                    학번
-                                </label>
-                                <input
-                                    id="student_id"
-                                    {...register('student_id')}
-                                    className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 transition ease-in-out hover:scale-105 px-2"
-                                />
-                            </div>
-                            <div className="max-w-md container mx-auto mb-8">
-                                <label htmlFor="student_password" className="font-medium">
-                                    비밀번호
-                                </label>
-                                <input
-                                    id="student_password"
-                                    {...register('student_password')}
-                                    className="border border-black max-w-md container mx-auto rounded-xl shadow-md h-10 transition ease-in-out hover:scale-105 px-2"
-                                    type="password"
-                                />
-                            </div>
-                            <button
-                                type="submit"
-                                className="shadow-md rounded-3xl h-[35px] w-[85px] bg-blue-500 items-center justify-center self-center text-base font-medium text-white hover:bg-blue-700 mx-auto transition ease-in-out hover:scale-110 flex"
-                            >
-                                로그인
-                            </button>
-                            {errMessage && <div className="text-center text-red-500 mt-2">{errMessage}</div>}
-                        </form>
-                        <Link
-                            className="h-[35px] w-[85px] items-center justify-center self-center flex mx-auto pt-4 text-base font-medium text-gray-500 sm:flex underline"
-                            to="/signup"
-                        >
-                            회원가입
-                        </Link>
-                    </div>
-                </>
-            ) : (
-                <UserPage userName={user} removeUserHandler={removeUserHandler} />
-            )} */}
         </>
     );
 };
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,60 +1,21 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import AdminPage from "./AdminPage";
 import UserPage from "./UserPage";
-import { useRecoilRefresher_UNSTABLE, useRecoilState } from "recoil";
-import userAuthInfoState, { studentInfoSelector } from "../state";
-import { useRecoilValue, useRecoilValueLoadable } from "recoil";
+import { useRecoilRefresher_UNSTABLE } from "recoil";
+import { studentInfoSelector } from "../state";
+import { useRecoilValueLoadable } from "recoil";
 import { userAuthInfoSelector } from "../state";
 
-const { Link } = require("react-router-dom");
-
 const MainPage = () => {
-    const [user, setUser] = useState("");
-    const [isLogin, setIsLogin] = useState(false);
-    const [isAdmin, setIsAdmin] = useState(false);
-    const [admin, setAdmin] = useState("");
-    // const [userAuthInfo, setUserAuthInfo] = useRecoilState(userAuthInfoState);
     const userAuthInfo = useRecoilValueLoadable(userAuthInfoSelector);
-    // const userAuthInfo = useRecoilValue(userAuthInfoSelector);
     const refreshUserAuthInfo = useRecoilRefresher_UNSTABLE(userAuthInfoSelector);
     const refreshStudentInfo = useRecoilRefresher_UNSTABLE(studentInfoSelector);
 
     useEffect(() => {
         refreshUserAuthInfo();
         refreshStudentInfo();
-        console.log(userAuthInfo);
-        // console.log(userAuthInfo.data);
-        // if (userAuthInfo.data.isLogin === 'False') {
-        //     alert('로그인이 필요합니다.');
-        //     window.location.href = '/';
-        // }
-        // if (userAuthInfo.isLogin === 'False') {
-        //     alert('로그인이 필요합니다.');
-        //     window.location.href = '/';
-        // }
-        // axios.get('http://localhost:4000/auth/authcheck', { withCredentials: true }).then((res) => {
-        //     console.log(res.data);
-        //     if (res.data.isLogin === 'True') {
-        //         if (res.data.isAdmin === true) {
-        //             setAdmin(res.data.studentId);
-        //             setIsLogin(true);
-        //             setIsAdmin(true);
-        //         } else {
-        //             setUser(res.data.studentName);
-        //             setIsAdmin(false);
-        //             setIsLogin(true);
-        //         }
-        //     } else {
-        //         alert('로그인이 필요합니다.');
-        //         window.location.href = '/';
-        //     }
-        // });
+        // console.log(userAuthInfo);
     }, []);
-
-    const removeUserHandler = () => {
-        setIsLogin(false);
-    };
 
     switch (userAuthInfo.state) {
         case "loading":
@@ -62,24 +23,12 @@ const MainPage = () => {
         case "hasError":
             return <div>error...</div>;
         default:
-            console.log(userAuthInfo.contents);
             if (userAuthInfo.contents.data.isLogin === "True" && userAuthInfo.contents.data.isAdmin) {
-                return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
+                return <AdminPage></AdminPage>;
             } else if (userAuthInfo.contents.data.isLogin === "True" && !userAuthInfo.contents.data.isAdmin) {
-                return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
+                return <UserPage></UserPage>;
             }
     }
-
-    // if (userAuthInfo.data.isLogin === 'True' && userAuthInfo.data.isAdmin) {
-    //     return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
-    // } else if (userAuthInfo.data.isLogin === 'True' && !userAuthInfo.data.isAdmin) {
-    //     return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
-    // }
-    // if (userAuthInfo.isLogin === 'True' && userAuthInfo.isAdmin === 'True') {
-    //     return <AdminPage admin={admin} removeUserHandler={removeUserHandler}></AdminPage>;
-    // } else if (userAuthInfo.isLogin === 'False' && userAuthInfo.isAdmin === 'False') {
-    //     return <UserPage userName={user} removeUserHandler={removeUserHandler}></UserPage>;
-    // }
 };
 
 export default MainPage;
