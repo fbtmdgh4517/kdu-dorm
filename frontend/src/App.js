6b90edf6ec074e471902cf828b0da3edc623fedb
seungho <fbtmdgh4517@naver.com>
2023-05-16 21:42:55 +0900

    식단 페이지 라우트 삭제, 점호 페이지 라우트 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import { Helmet } from 'react-helmet-async';
-import { Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import AdminPage from './pages/AdminPage';
-import NoticePage from './pages/NoticePage';
-import SignupPage from './pages/SignupPage';
-import SinchungCheckPage from './pages/SinchungCheckPage';
-import SinchungPage from './pages/SinchungPage';
-import UserPage from './pages/UserPage';
-import WeekDiet from './pages/WeekDiet';
-import MainPage from './pages/MainPage';
-import SignupCheckPage from './pages/SignUpCheckPage';
-import { RecoilRoot } from 'recoil';
-import PointPage from './pages/PointPage';
+import { Helmet } from "react-helmet-async";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import AdminPage from "./pages/AdminPage";
+import NoticePage from "./pages/NoticePage";
+import SignupPage from "./pages/SignupPage";
+import SinchungCheckPage from "./pages/SinchungCheckPage";
+import SinchungPage from "./pages/SinchungPage";
+import UserPage from "./pages/UserPage";
+import MainPage from "./pages/MainPage";
+import SignupCheckPage from "./pages/SignUpCheckPage";
+import { RecoilRoot } from "recoil";
+import PointPage from "./pages/PointPage";
+import RollCallPage from "./pages/RollCallPage";
 
 function App() {
     return (
@@ -28,9 +28,9 @@ function App() {
                 <Route path="/managerMain" element={<AdminPage />} />
                 <Route path="/sinchungCheck/:id" element={<SinchungCheckPage />} />
                 <Route path="/signupRequestCheck/:id" element={<SignupCheckPage />} />
-                <Route path="/weekDiet" element={<WeekDiet />} />
                 <Route path="/notice" element={<NoticePage />} />
                 <Route path="/managePoint" element={<PointPage />} />
+                <Route path="/rollCall" element={<RollCallPage />} />
             </Routes>
         </RecoilRoot>
     );
