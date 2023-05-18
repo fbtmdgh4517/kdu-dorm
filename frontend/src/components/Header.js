a5c8b7f7a5a2d6b392f5ea35907873e6f64b3075
seungho <fbtmdgh4517@naver.com>
2023-05-18 23:37:12 +0900

    Link 태그에 학교 기숙사 이미지 추가

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,54 +1,44 @@
 import { Link } from "react-router-dom";
+import logo from "../images/metrodorm_logo.png";
 
 const Header = ({ userName, onSideBarToggle }) => {
-    return (
-        <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
-            <div className="px-3 py-5 lg:px-5 lg:pl-3">
-                <div className="flex items-center justify-between">
-                    <div className="flex items-center justify-start">
-                        <button
-                            id="toggleSidebarMobile"
-                            aria-expanded="true"
-                            aria-controls="sidebar"
-                            onClick={onSideBarToggle}
-                            className="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
-                        >
-                            <svg
-                                id="toggleSidebarMobileHamburger"
-                                className="w-6 h-6"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
-                            <svg
-                                id="toggleSidebarMobileClose"
-                                className="w-6 h-6 hidden"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
-                        </button>
-                        <Link className="text-xl font-bold flex items-center lg:ml-2.5" to="/main">
-                            경동대학교 기숙사 외박신청
-                        </Link>
-                    </div>
-                    <div className="flex items-center">{userName}</div>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="bg-white border-b-2 border-gray-200 fixed z-30 w-full">
+      <div className="px-3 py-3 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start">
+            <button
+              id="toggleSidebarMobile"
+              aria-expanded="true"
+              aria-controls="sidebar"
+              onClick={onSideBarToggle}
+              className="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
+            >
+              <svg id="toggleSidebarMobileHamburger" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <svg id="toggleSidebarMobileClose" className="w-6 h-6 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <Link className="text-xl font-bold flex items-center lg:ml-2.5" to="/main">
+              <img src={logo} alt="logo" className="max-w-[200px] md:max-w-[250px]" />
+              <span className="font-bold text-gray-900 text-base md:text-xl">외박신청</span>
+            </Link>
+          </div>
+          <div className="flex items-center">{userName}</div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
