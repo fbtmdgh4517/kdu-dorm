b92c8e5c9b089cdba949956146febe0a83d3508c
seungho <fbtmdgh4517@naver.com>
2023-05-18 23:37:36 +0900

    배경 색 수정

--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -7,44 +7,44 @@ import NoticeListContainer from "../containers/NoticeListContainer";
 import PenaltyDangerStudentsListContainer from "../containers/PenaltyDangerStudentsListContainer";
 
 const AdminPage = () => {
-    return (
-        <>
-            <HeaderContainer></HeaderContainer>
-            <div className="flex overflow-hidden bg-white pt-16">
-                <SidebarContainer></SidebarContainer>
-                <div id="main-content" className="h-full w-full bg-gray-100 relative overflow-y-auto lg:ml-64">
-                    <main>
-                        <div className="py-6 px-4">
-                            <div className="w-full grid grid-cols-1 xl:grid-cols-3 gap-4">
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                                    {/* 카드 */}
-                                    <UsersApplicationListContainer></UsersApplicationListContainer>
-                                </div>
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                                    {/* 카드 */}
-                                    <SignupRequestListContainer></SignupRequestListContainer>
-                                </div>
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6">
-                                    {/* 카드 */}
-                                    <TodayMenuContainer></TodayMenuContainer>
-                                </div>
-                            </div>
-                            <div className="grid grid-cols-1 2xl:grid-cols-2 gap-4 my-4">
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                                    {/* 카드 */}
-                                    <NoticeListContainer></NoticeListContainer>
-                                </div>
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                                    {/* 카드 */}
-                                    <PenaltyDangerStudentsListContainer></PenaltyDangerStudentsListContainer>
-                                </div>
-                            </div>
-                        </div>
-                    </main>
+  return (
+    <>
+      <HeaderContainer></HeaderContainer>
+      <div className="flex overflow-hidden bg-white pt-16">
+        <SidebarContainer></SidebarContainer>
+        <div id="main-content" className="h-full w-full bg-blue-100 relative overflow-y-auto lg:ml-64">
+          <main>
+            <div className="py-10 px-4">
+              <div className="w-full grid grid-cols-1 xl:grid-cols-3 gap-4">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {/* 카드 */}
+                  <UsersApplicationListContainer></UsersApplicationListContainer>
                 </div>
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {/* 카드 */}
+                  <SignupRequestListContainer></SignupRequestListContainer>
+                </div>
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6">
+                  {/* 카드 */}
+                  <TodayMenuContainer></TodayMenuContainer>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 2xl:grid-cols-2 gap-4 my-4">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {/* 카드 */}
+                  <NoticeListContainer></NoticeListContainer>
+                </div>
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {/* 카드 */}
+                  <PenaltyDangerStudentsListContainer></PenaltyDangerStudentsListContainer>
+                </div>
+              </div>
             </div>
-        </>
-    );
+          </main>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdminPage;
--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -6,40 +6,40 @@ import TodayMenuContainer from "../containers/TodayMenuContainer";
 import UserPointInfoContainer from "../containers/UserPointInfoContainer";
 
 const UserPage = () => {
-    return (
-        <>
-            <HeaderContainer></HeaderContainer>
-            <div className="flex overflow-hidden bg-white pt-16">
-                <SidebarContainer></SidebarContainer>
-                <div id="main-content" className="h-full w-full bg-gray-100 relative overflow-y-auto lg:ml-64">
-                    <main>
-                        <div className="py-6 px-4">
-                            <div className="w-full grid grid-cols-1 xl:grid-cols-3 gap-4">
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                                    {/* 카드 */}
-                                    <OwnApplicationListContainer></OwnApplicationListContainer>
-                                </div>
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                                    {/* 카드 */}
-                                    <NoticeListContainer></NoticeListContainer>
-                                </div>
-                                <div className="bg-white shadow rounded-lg mb-4 p-4 sm:p-6">
-                                    {/* 카드 */}
-                                    <TodayMenuContainer></TodayMenuContainer>
-                                </div>
-                            </div>
-                            <div className="grid grid-cols-1 2xl:grid-cols-1 gap-4 my-4">
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                                    {/* 카드 */}
-                                    <UserPointInfoContainer></UserPointInfoContainer>
-                                </div>
-                            </div>
-                        </div>
-                    </main>
+  return (
+    <>
+      <HeaderContainer></HeaderContainer>
+      <div className="flex overflow-hidden bg-white pt-16">
+        <SidebarContainer></SidebarContainer>
+        <div id="main-content" className="h-full w-full bg-blue-100 relative overflow-y-auto lg:ml-64">
+          <main>
+            <div className="py-10 px-4">
+              <div className="w-full grid grid-cols-1 xl:grid-cols-3 gap-4">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {/* 카드 */}
+                  <OwnApplicationListContainer></OwnApplicationListContainer>
                 </div>
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {/* 카드 */}
+                  <NoticeListContainer></NoticeListContainer>
+                </div>
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6">
+                  {/* 카드 */}
+                  <TodayMenuContainer></TodayMenuContainer>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 2xl:grid-cols-1 gap-4 my-4">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {/* 카드 */}
+                  <UserPointInfoContainer></UserPointInfoContainer>
+                </div>
+              </div>
             </div>
-        </>
-    );
+          </main>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserPage;
