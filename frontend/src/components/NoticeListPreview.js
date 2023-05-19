134039f9b1d963caa95b60529d495e55b22b819c
seungho <fbtmdgh4517@naver.com>
2023-05-19 23:53:16 +0900

    ui 수정

--- frontend/src/components/NoticeListPreview.js
+++ frontend/src/components/NoticeListPreview.js
@@ -1,9 +1,47 @@
 import Pagination from "react-js-pagination";
 
 const NoticeListPreview = ({ notice, page, pageChangeHandler, offset }) => {
-    return (
-        <>
-            {notice &&
+  return (
+    <>
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-xl font-bold leading-none text-gray-900">기숙사 공지사항</h1>
+      </div>
+      <div className="p-3">
+        <div className="overflow-x-auto rounded-lg border border-blue-500 shadow-md">
+          <table className="table-auto w-full">
+            <thead className="font-semibold text-gray-700 bg-blue-100">
+              <tr>
+                <th className="p-2 whitespace-nowrap">
+                  <div className="font-semibold ">제목</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div className="font-semibold ">작성자</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div className="font-semibold ">작성일</div>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 pl-2">
+              {notice &&
+                notice.slice(offset, offset + 8).map((su) => {
+                  return (
+                    <div key={su.seq} className="">
+                      <a
+                        href={`https://metrodorm.kduniv.ac.kr/bbs/getBbsWriteView.kmc?seq=${su.seq}&bbs_locgbn=KY&bbs_id=notice`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {su.subject}
+                      </a>
+                    </div>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {/* {notice &&
                 notice.slice(offset, offset + 8).map((su) => {
                     return (
                         <div key={su.seq}>
@@ -17,27 +55,27 @@ const NoticeListPreview = ({ notice, page, pageChangeHandler, offset }) => {
                             <br></br>
                         </div>
                     );
-                })}
-            {notice && (
-                <div className="mx-auto items-center flex justify-center mt-6">
-                    <Pagination
-                        innerClass="inline-flex items-center -space-x-px"
-                        itemClassFirst="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
-                        itemClassLast="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
-                        itemClass="px-3 py-2 leading-tight border border-gray-300"
-                        activeClass="z-10 px-3 py-2 leading-tight bg-blue-500 text-white border border-blue-300"
-                        activePage={page}
-                        itemsCountPerPage={8}
-                        totalItemsCount={notice.length}
-                        pageRangeDisplayed={5}
-                        prevPageText={"<"}
-                        nextPageText={">"}
-                        onChange={pageChangeHandler}
-                    />
-                </div>
-            )}
-        </>
-    );
+                })} */}
+      {notice && (
+        <div className="mx-auto items-center flex justify-center mt-6">
+          <Pagination
+            innerClass="inline-flex items-center -space-x-px"
+            itemClassFirst="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
+            itemClassLast="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
+            itemClass="px-3 py-2 leading-tight border border-gray-300"
+            activeClass="z-10 px-3 py-2 leading-tight bg-blue-500 text-white border border-blue-300"
+            activePage={page}
+            itemsCountPerPage={8}
+            totalItemsCount={notice.length}
+            pageRangeDisplayed={5}
+            prevPageText={"<"}
+            nextPageText={">"}
+            onChange={pageChangeHandler}
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default NoticeListPreview;
--- frontend/src/components/PenaltyDangerStudentsList.js
+++ frontend/src/components/PenaltyDangerStudentsList.js
@@ -1,44 +1,67 @@
 import Pagination from "react-js-pagination";
 
 const PenaltyDangerStudentsList = ({ dangerStudentList, page, pageChangeHandler, offset }) => {
-    return (
-        <>
-            <div className="flex items-center justify-between mb-4">
-                <h1 className="text-xl font-bold leading-none text-gray-900">벌점 위험 학생 목록</h1>
-            </div>
-            {dangerStudentList.slice(offset, offset + 5).map((student) => {
+  return (
+    <>
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-xl font-bold leading-none text-gray-900">벌점 위험 학생 목록</h1>
+      </div>
+      <div className="p-3">
+        <div className="overflow-x-auto rounded-lg border border-blue-500 shadow-md">
+          <table className="table-auto w-full">
+            <thead className="font-semibold text-gray-700 bg-blue-100">
+              <tr>
+                <th className="p-2 whitespace-nowrap">
+                  <div className="font-semibold ">학생</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div className="font-semibold ">호실</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div className="font-semibold ">현재 벌점</div>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 text-center">
+              {dangerStudentList.slice(offset, offset + 5).map((student) => {
                 return (
-                    <div key={student.student_id} className="flex m-3 justify-between">
-                        <div>
-                            <span>
-                                {student.student_id} {student.student_name} ({student.student_room}호)
-                            </span>
+                  <tr key={student.student_id}>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="">
+                        <div className="font-medium text-gray-800">
+                          {student.student_id} {student.student_name}
                         </div>
-                        <div>
-                            <span>현재 벌점 : </span>
-                            <span className="font-medium text-red-600">{student.penalty_point}점</span>
-                        </div>
-                    </div>
+                      </div>
+                    </td>
+                    <td className="p-2 whitespace-nowrap">{student.student_room}호</td>
+                    <td className="p-2 whitespace-nowrap">
+                      <span className="font-medium text-red-600">{student.penalty_point}점</span>
+                    </td>
+                  </tr>
                 );
-            })}
-            <div className="mx-auto items-center flex justify-center mt-6">
-                <Pagination
-                    innerClass="inline-flex items-center -space-x-px bg-white"
-                    itemClassFirst="block px-3 py-2 ml-0 leading-tight border border-gray-300 rounded-l-lg"
-                    itemClassLast="block px-3 py-2 leading-tight border border-gray-300 rounded-r-lg"
-                    itemClass="block px-3 py-2 leading-tight border border-gray-300 cursor-pointer hover:bg-blue-500 hover:text-white"
-                    activeClass="z-10 px-3 py-2 leading-tight bg-blue-500 text-white border border-blue-300"
-                    activePage={page}
-                    itemsCountPerPage={5}
-                    totalItemsCount={dangerStudentList.length}
-                    pageRangeDisplayed={5}
-                    prevPageText={"<"}
-                    nextPageText={">"}
-                    onChange={pageChangeHandler}
-                />
-            </div>
-        </>
-    );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="mx-auto items-center flex justify-center mt-6">
+        <Pagination
+          innerClass="inline-flex items-center -space-x-px bg-white"
+          itemClassFirst="block px-3 py-2 ml-0 leading-tight border border-gray-300 rounded-l-lg"
+          itemClassLast="block px-3 py-2 leading-tight border border-gray-300 rounded-r-lg"
+          itemClass="block px-3 py-2 leading-tight border border-gray-300 cursor-pointer hover:bg-blue-500 hover:text-white"
+          activeClass="z-10 px-3 py-2 leading-tight bg-blue-500 text-white border border-blue-300"
+          activePage={page}
+          itemsCountPerPage={5}
+          totalItemsCount={dangerStudentList.length}
+          pageRangeDisplayed={5}
+          prevPageText={"<"}
+          nextPageText={">"}
+          onChange={pageChangeHandler}
+        />
+      </div>
+    </>
+  );
 };
 
 export default PenaltyDangerStudentsList;
