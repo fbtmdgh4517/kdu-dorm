a06770a2ae314ac26f36d150b79c5dfbf625683b
seungho <fbtmdgh4517@naver.com>
2023-05-21 23:58:22 +0900

    학교 기숙사 공지사항 페이지의 1페이지 공지사항만 가져오는 문제 수정

--- backend/src/routes/notice.js
+++ backend/src/routes/notice.js
@@ -1,61 +1,52 @@
-const express = require('express');
-const FormData = require('form-data');
-const axios = require('axios');
-const request = require('request');
+const express = require("express");
+const FormData = require("form-data");
+const axios = require("axios");
+const request = require("request");
 const form = new FormData();
 const router = express.Router();
 
-form.append('rows', '10');
-form.append('bbs_locgbn', 'KY');
-form.append('bbs_id', 'notice');
-form.append('cPage', '1');
+form.append("rows", "10");
+form.append("bbs_locgbn", "KY");
+form.append("bbs_id", "notice");
+form.append("cPage", "1");
 
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
-router.get('/', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    axios
-        .post('https://metrodorm.kduniv.ac.kr/bbs/getBbsList.kmc', {
-            rows: '10',
-            bbs_locgbn: 'KY',
-            bbs_id: 'notice',
-            cPage: '1',
-        })
-        .then((response) => {
-            // console.log(response.data);
-            res.send(response.data);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('서버 오류');
+router.get("/", async (req, res) => {
+  const noticeList = [];
+
+  try {
+    const res = await axios.post("https://metrodorm.kduniv.ac.kr/bbs/getBbsList.kmc", {
+      rows: "10",
+      bbs_locgbn: "KY",
+      bbs_id: "notice",
+      cPage: "1",
+    });
+
+    noticeList.push(...res.data.root[0].topList, ...res.data.root[0].list);
+
+    const totalPages = Math.ceil(res.data.root[0].totalCount[0].cnt / 10);
+
+    for (let i = 2; i <= totalPages; i++) {
+      try {
+        const res = await axios.post("https://metrodorm.kduniv.ac.kr/bbs/getBbsList.kmc", {
+          rows: "10",
+          bbs_locgbn: "KY",
+          bbs_id: "notice",
+          cPage: i.toString(),
         });
-    // axios
-    //     .post('https://metrodorm.kduniv.ac.kr/bbs/getBbsList.kmc', form, {
-    //         headers: {},
-    //     })
-    //     .then((response) => {
-    //         console.log(response.data);
-    //         // res.send(response.data);
-    //     })
-    //     .catch((error) => {
-    //         console.log(error);
-    //         res.status(500).send('서버 오류');
-    //     });
-    // let options = {
-    //     method: 'POST',
-    //     url: 'https://metrodorm.kduniv.ac.kr/bbs/getBbsList.kmc',
-    //     headers: {},
-    //     formData: {
-    //         rows: '10',
-    //         bbs_locgbn: 'KY',
-    //         bbs_id: 'notice',
-    //         cPage: '1',
-    //     },
-    // };
-    // request(options, (error, response) => {
-    //     if (error) throw new Error(error);
-    //     res.send(response.body);
-    // });
+
+        noticeList.push(...res.data.root[0].list);
+      } catch (err) {
+        console.log(err);
+        res.status(500).send("서버 오류");
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("서버 오류");
+  }
+  res.send(noticeList.sort((a, b) => b.seq - a.seq));
 });
 
 module.exports = router;
