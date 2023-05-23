9955a42d6931d89b084c537f2f86351e7d4de7df
seungho <fbtmdgh4517@naver.com>
2023-05-23 22:43:51 +0900

    학생 목록 selector 새로고침하는 기능 추가

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,33 +2,35 @@ import { useEffect, useState } from "react";
 import AdminPage from "./AdminPage";
 import UserPage from "./UserPage";
 import { useRecoilRefresher_UNSTABLE } from "recoil";
-import { studentInfoSelector } from "../state";
+import { studentInfoSelector, studentListSelector } from "../state";
 import { useRecoilValueLoadable } from "recoil";
 import { userAuthInfoSelector } from "../state";
 
 const MainPage = () => {
-    const userAuthInfo = useRecoilValueLoadable(userAuthInfoSelector);
-    const refreshUserAuthInfo = useRecoilRefresher_UNSTABLE(userAuthInfoSelector);
-    const refreshStudentInfo = useRecoilRefresher_UNSTABLE(studentInfoSelector);
+  const userAuthInfo = useRecoilValueLoadable(userAuthInfoSelector);
+  const refreshUserAuthInfo = useRecoilRefresher_UNSTABLE(userAuthInfoSelector);
+  const refreshStudentInfo = useRecoilRefresher_UNSTABLE(studentInfoSelector);
+  const refreshStudentList = useRecoilRefresher_UNSTABLE(studentListSelector);
 
-    useEffect(() => {
-        refreshUserAuthInfo();
-        refreshStudentInfo();
-        // console.log(userAuthInfo);
-    }, []);
+  useEffect(() => {
+    refreshUserAuthInfo();
+    refreshStudentInfo();
+    refreshStudentList();
+    // console.log(userAuthInfo);
+  }, []);
 
-    switch (userAuthInfo.state) {
-        case "loading":
-            return <div>loading...</div>;
-        case "hasError":
-            return <div>error...</div>;
-        default:
-            if (userAuthInfo.contents.data.isLogin === "True" && userAuthInfo.contents.data.isAdmin) {
-                return <AdminPage></AdminPage>;
-            } else if (userAuthInfo.contents.data.isLogin === "True" && !userAuthInfo.contents.data.isAdmin) {
-                return <UserPage></UserPage>;
-            }
-    }
+  switch (userAuthInfo.state) {
+    case "loading":
+      return <div>loading...</div>;
+    case "hasError":
+      return <div>error...</div>;
+    default:
+      if (userAuthInfo.contents.data.isLogin === "True" && userAuthInfo.contents.data.isAdmin) {
+        return <AdminPage></AdminPage>;
+      } else if (userAuthInfo.contents.data.isLogin === "True" && !userAuthInfo.contents.data.isAdmin) {
+        return <UserPage></UserPage>;
+      }
+  }
 };
 
 export default MainPage;
