cabd7a6e80db3374707a2808c8a7c8929db6c6cf
seungho <fbtmdgh4517@naver.com>
2023-05-24 23:34:17 +0900

    통계 라우터 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,21 +13,22 @@ const authRouter = require("./routes/auth");
 const applicationRouter = require("./routes/application");
 const studentsRouter = require("./routes/students");
 const rollCallRouter = require("./routes/rollCall");
+const statisticsRouter = require("./routes/statistics");
 
 const envPath = path.join(__dirname, "../.env");
 require("dotenv").config({ path: envPath });
 const { COOKIE_SECRET, PORT, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const options = {
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 
-    clearExpired: true,
-    checkExpirationInterval: 10000,
-    expiration: 60 * 60 * 1000 * 2,
+  clearExpired: true,
+  checkExpirationInterval: 10000,
+  expiration: 60 * 60 * 1000 * 2,
 };
 
 const sessionStore = new MySQLStore(options);
@@ -42,25 +43,25 @@ const sessionStore = new MySQLStore(options);
 //     });
 
 app.use(
-    session({
-        resave: false,
-        saveUninitialized: false,
-        secret: COOKIE_SECRET,
-        store: sessionStore,
-        cookie: {
-            httpOnly: true,
-            secure: false,
-            maxAge: 60 * 60 * 1000 * 24,
-        },
-        name: "session-cookie",
-    })
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: COOKIE_SECRET,
+    store: sessionStore,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 60 * 60 * 1000 * 24,
+    },
+    name: "session-cookie",
+  })
 );
 
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
 );
 app.use("/", express.static(path.join(__dirname, "../../frontend/build")));
 app.use(express.json());
@@ -75,15 +76,16 @@ app.use("/auth", authRouter);
 app.use("/application", applicationRouter);
 app.use("/students", studentsRouter);
 app.use("/rollCall", rollCallRouter);
+app.use("/statistics", statisticsRouter);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
 });
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
 });
 
 const port = PORT || 4000;
 app.listen(port, () => {
-    console.log(port, "번 포트에서 대기 중");
+  console.log(port, "번 포트에서 대기 중");
 });
