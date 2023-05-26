a84f8e2254a1b63cb80d5b15752b8baf0eff6051
seungho <fbtmdgh4517@naver.com>
2023-05-26 23:02:10 +0900

    기간별 일일 비교 통계 라우터 추가

--- backend/src/routes/statistics.js
+++ backend/src/routes/statistics.js
@@ -7,18 +7,8 @@ router.get("/todayStatistics", (req, res) => {
   const year = todayDate.getFullYear();
   const month = todayDate.getMonth() + 1;
   const day = todayDate.getDate();
-  const sendData = {
-    todayApplicationCount: 0,
-    todayApplicationAcceptCount: 0,
-    todayApplicationRejectCount: 0,
-    todayRollcallCompletePersonnelCount: 0,
-    todayRollcallOvernightPersonnelCount: 0,
-    todayRollcallUncompletePersonnelCount: 0,
-    todayPenaltyPointPersonnelCount: 0,
-    todayBonusPointPersonnelCount: 0,
-  };
 
-  const sendData2 = [];
+  const sendData = [];
 
   const query =
     `SELECT COUNT(*) FROM overnight_applications WHERE YEAR(application_time) = ${year} AND MONTH(application_time) = ${month} AND DAY(application_time) = ${day} ` +
@@ -39,24 +29,273 @@ router.get("/todayStatistics", (req, res) => {
 
   db.query(query, (error, results) => {
     if (error) throw error;
+    sendData.push({ id: "외박 신청 수", value: results[0]["COUNT(*)"] });
+    sendData.push({ id: "외박 신청 승인 수", value: results[1]["COUNT(*)"] });
+    sendData.push({ id: "외박 신청 거부 수", value: results[2]["COUNT(*)"] });
+    sendData.push({ id: "점호 완료 인원 수", value: results[3]["COUNT(*)"] });
+    sendData.push({ id: "외박 인원 수", value: results[4]["COUNT(*)"] });
+    sendData.push({ id: "무단 외박 인원 수", value: results[5]["COUNT(*)"] });
+    sendData.push({ id: "벌점 받은 인원 수", value: results[6]["COUNT(*)"] });
+    sendData.push({ id: "상점 받은 인원 수", value: results[7]["COUNT(*)"] });
+    console.log(sendData);
+    res.send(sendData);
+  });
+});
+
+router.get("/compareStatistics/:day", (req, res) => {
+  const day = req.params.day;
+  const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+  const today = new Date();
+  const query =
+    "SELECT record_date, dayofweek(record_date) as _day, count(*), is_checked " +
+    "FROM rollcall_records " +
+    "WHERE is_checked = '무단외박' " +
+    `AND record_date >= CURRENT_DATE() - INTERVAL ${day} DAY ` +
+    "GROUP BY dayofweek(record_date), record_date " +
+    "UNION ALL " +
+    "SELECT record_date, dayofweek(record_date) as _day, count(*), is_checked " +
+    "FROM rollcall_records " +
+    "WHERE is_checked = '외박' " +
+    `AND record_date >= CURRENT_DATE() - INTERVAL ${day} DAY ` +
+    "GROUP BY dayofweek(record_date), record_date " +
+    "UNION ALL " +
+    "SELECT record_date, dayofweek(record_date) as _day, count(*), is_checked " +
+    "FROM rollcall_records " +
+    "WHERE is_checked = '완료' " +
+    `AND record_date >= CURRENT_DATE() - INTERVAL ${day} DAY ` +
+    "GROUP BY dayofweek(record_date), record_date " +
+    "ORDER BY record_date";
+
+  const applicationQuery =
+    "SELECT date(application_time) application_date, dayofweek(date(application_time)) as _day, count(*), approval_status " +
+    "FROM overnight_applications " +
+    "WHERE approval_status = '승인' " +
+    `AND application_time >= CURRENT_DATE() - INTERVAL ${day} DAY ` +
+    "GROUP BY dayofweek(date(application_time)), date(application_time) " +
+    "UNION ALL " +
+    "SELECT date(application_time), dayofweek(date(application_time)) as _day, count(*), approval_status " +
+    "FROM overnight_applications " +
+    "WHERE approval_status = '거부' " +
+    `AND application_time >= CURRENT_DATE() - INTERVAL ${day} DAY ` +
+    "GROUP BY dayofweek(date(application_time)), date(application_time) " +
+    "ORDER BY application_date";
+
+  const sendData = {
+    rollCallCompareStatistics: [],
+    applicationCompareStatistics: [],
+  };
+
+  db.query(query, (error, results) => {
+    if (error) throw error;
+    sendData.rollCallCompareStatistics = results;
+  });
+
+  db.query(applicationQuery, (error, results) => {
+    if (error) throw error;
+    sendData.applicationCompareStatistics = results;
+    res.send(sendData);
+  });
+});
+
+router.get("/statistics/:day", (req, res) => {
+  const day = req.params.day;
+  const applicationQuery =
+    "SELECT count(*) as value " +
+    "FROM overnight_applications " +
+    `WHERE application_time BETWEEN date_add(now(), INTERVAL -${day} DAY) AND now() ` +
+    "AND approval_status = '승인' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM overnight_applications " +
+    `WHERE application_time BETWEEN date_add(now(), INTERVAL -${day} DAY) AND now() ` +
+    "AND approval_status = '거부' ";
+
+  const rollCallQuery =
+    "SELECT count(*) as value " +
+    "FROM rollcall_records " +
+    `WHERE record_date BETWEEN date_add(now(), INTERVAL -${day} DAY) AND now() ` +
+    "AND is_checked = '완료' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM rollcall_records " +
+    `WHERE record_date BETWEEN date_add(now(), INTERVAL -${day} DAY) AND now() ` +
+    "AND is_checked = '외박' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM rollcall_records " +
+    `WHERE record_date BETWEEN date_add(now(), INTERVAL -${day} DAY) AND now() ` +
+    "AND is_checked = '무단외박' ";
+
+  const pointQuery =
+    "SELECT count(*) as value " +
+    "FROM score_records " +
+    `WHERE score_date BETWEEN date_add(now(), INTERVAL -${day} DAY) AND now() ` +
+    "AND score_type = '상점' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM score_records " +
+    `WHERE score_date BETWEEN date_add(now(), INTERVAL -${day} DAY) AND now() ` +
+    "AND score_type = '벌점'";
+
+  const query = applicationQuery + "UNION ALL " + rollCallQuery + "UNION ALL " + pointQuery;
+
+  const sendData = {
+    applicationStatistics: {
+      "승인된 외박신청 수": 0,
+      "거부된 외박신청 수": 0,
+    },
+    rollCallStatistics: {
+      "점호 완료 인원 수": 0,
+      "외박 인원 수": 0,
+      "무단 외박 인원 수": 0,
+    },
+    pointStatistics: {
+      "상점 부여 횟수": 0,
+      "벌점 부여 횟수": 0,
+    },
+  };
+
+  db.query(query, (error, results) => {
+    if (error) throw error;
+    sendData.applicationStatistics["승인된 외박신청 수"] = results[0];
+    sendData.applicationStatistics["거부된 외박신청 수"] = results[1];
+    sendData.rollCallStatistics["점호 완료 인원 수"] = results[2];
+    sendData.rollCallStatistics["외박 인원 수"] = results[3];
+    sendData.rollCallStatistics["무단 외박 인원 수"] = results[4];
+    sendData.pointStatistics["상점 부여 횟수"] = results[5];
+    sendData.pointStatistics["벌점 부여 횟수"] = results[6];
+    res.send(sendData);
+  });
+});
+
+router.get("/monthlyStatistics", (req, res) => {
+  const applicationQuery =
+    "SELECT count(*) as value " +
+    "FROM overnight_applications " +
+    "WHERE application_time BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND approval_status = '승인' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM overnight_applications " +
+    "WHERE application_time BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND approval_status = '거부' ";
+
+  const rollCallQuery =
+    "SELECT count(*) as value " +
+    "FROM rollcall_records " +
+    "WHERE record_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND is_checked = '완료' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM rollcall_records " +
+    "WHERE record_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND is_checked = '외박' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM rollcall_records " +
+    "WHERE record_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND is_checked = '무단외박' ";
+
+  const pointQuery =
+    "SELECT count(*) as value " +
+    "FROM score_records " +
+    "WHERE score_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND score_type = '상점' " +
+    "UNION ALL " +
+    "SELECT count(*) as value " +
+    "FROM score_records " +
+    "WHERE score_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND score_type = '벌점'";
+
+  const query = applicationQuery + "UNION ALL " + rollCallQuery + "UNION ALL " + pointQuery;
+
+  const sendData = {
+    applicationStatistics: {
+      "승인된 외박신청 수": 0,
+      "거부된 외박신청 수": 0,
+    },
+    rollCallStatistics: {
+      "점호 완료 인원 수": 0,
+      "외박 인원 수": 0,
+      "무단 외박 인원 수": 0,
+    },
+    pointStatistics: {
+      "상점 부여 횟수": 0,
+      "벌점 부여 횟수": 0,
+    },
+  };
+
+  db.query(query, (error, results) => {
+    if (error) throw error;
+    sendData.applicationStatistics["승인된 외박신청 수"] = results[0];
+    sendData.applicationStatistics["거부된 외박신청 수"] = results[1];
+    sendData.rollCallStatistics["점호 완료 인원 수"] = results[2];
+    sendData.rollCallStatistics["외박 인원 수"] = results[3];
+    sendData.rollCallStatistics["무단 외박 인원 수"] = results[4];
+    sendData.pointStatistics["상점 부여 횟수"] = results[5];
+    sendData.pointStatistics["벌점 부여 횟수"] = results[6];
+    res.send(sendData);
+  });
+});
+
+router.get("/weeklyCompareStatistics", (req, res) => {
+  const todayDate = new Date();
+  const year = todayDate.getFullYear();
+  const month = todayDate.getMonth() + 1;
+  const date = todayDate.getDate();
+
+  const sendData = {
+    applicationStatistics: [],
+    rollCallStatistics: [],
+  };
+
+  const applicationQuery =
+    "SELECT '외박신청 승인 수' as id, date(application_time) application_date, count(*) " +
+    "FROM overnight_applications " +
+    "WHERE application_time BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND approval_status = '승인' " +
+    "GROUP BY date(application_time) " +
+    "UNION ALL " +
+    "SELECT '외박신청 거부 수' as id, date(application_time) application_date, count(*) " +
+    "FROM overnight_applications " +
+    "WHERE application_time BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND approval_status = '거부' " +
+    "GROUP BY date(application_time) " +
+    "ORDER BY application_date";
+
+  const rollCallQuery =
+    "SELECT '외박 인원 수' as id, record_date, count(*) " +
+    "FROM rollcall_records " +
+    "WHERE record_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND is_checked = '외박' " +
+    "GROUP BY record_date " +
+    "UNION ALL " +
+    "SELECT '무단 외박 인원 수' as id, record_date, count(*) " +
+    "FROM rollcall_records " +
+    "WHERE record_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND is_checked = '무단외박' " +
+    "GROUP BY record_date " +
+    "UNION ALL " +
+    "SELECT '점호 완료 인원 수' as id, record_date, count(*) " +
+    "FROM rollcall_records " +
+    "WHERE record_date BETWEEN date_add(now(), INTERVAL -1 WEEK) AND now() " +
+    "AND is_checked = '완료' " +
+    "GROUP BY record_date " +
+    "ORDER BY record_date";
+
+  db.query(applicationQuery, (error, results) => {
+    if (error) throw error;
     // console.log(results);
-    // sendData.todayApplicationCount = results[0]["COUNT(*)"];
-    // sendData.todayApplicationAcceptCount = results[1]["COUNT(*)"];
-    // sendData.todayApplicationRejectCount = results[2]["COUNT(*)"];
-    // sendData.todayRollcallCompletePersonnelCount = results[3]["COUNT(*)"];
-    // sendData.todayRollcallOvernightPersonnelCount = results[4]["COUNT(*)"];
-    // sendData.todayRollcallUncompletePersonnelCount = results[5]["COUNT(*)"];
-    // sendData.todayPenaltyPointPersonnelCount = results[6]["COUNT(*)"];
-    // sendData.todayBonusPointPersonnelCount = results[7]["COUNT(*)"];
-    sendData2.push({ id: "외박 신청 수", value: results[0]["COUNT(*)"] });
-    sendData2.push({ id: "외박 신청 승인 수", value: results[1]["COUNT(*)"] });
-    sendData2.push({ id: "외박 신청 거부 수", value: results[2]["COUNT(*)"] });
-    sendData2.push({ id: "점호 완료 인원 수", value: results[3]["COUNT(*)"] });
-    sendData2.push({ id: "외박 인원 수", value: results[4]["COUNT(*)"] });
-    sendData2.push({ id: "무단 외박 인원 수", value: results[5]["COUNT(*)"] });
-    sendData2.push({ id: "벌점 받은 인원 수", value: results[6]["COUNT(*)"] });
-    sendData2.push({ id: "상점 받은 인원 수", value: results[7]["COUNT(*)"] });
-    res.send(sendData2);
+    sendData.applicationStatistics = results;
+    // res.send(sendData);
+  });
+
+  db.query(rollCallQuery, (error, results) => {
+    if (error) throw error;
+    // console.log(results);
+    sendData.rollCallStatistics = results;
+    console.log(sendData);
+    res.send(sendData);
   });
 });
 
