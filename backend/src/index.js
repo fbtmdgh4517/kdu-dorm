b825a5cd6f23460d8cb468e96b21636530b82b93
seungho <fbtmdgh4517@naver.com>
2023-06-02 01:08:30 +0900

    cors 모듈 origin 수정

--- backend/src/index.js
+++ backend/src/index.js
@@ -58,7 +58,8 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://3.39.123.237:4000", "http://localhost:4000", "http://3.39.123.237:8080"],
+    origin: "http://3.39.123.237:4000",
+    // origin: ["http://3.39.123.237:4000", "http://localhost:4000", "http://3.39.123.237:8080", "http://localhost:3000"],
     credentials: true,
   })
 );
