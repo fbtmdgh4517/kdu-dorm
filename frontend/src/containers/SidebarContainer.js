54a0980af525f540fc26e13358490aa6c7eca0be
seungho <fbtmdgh4517@naver.com>
2023-06-04 23:41:09 +0900

    로그아웃 시 사용자 인증 정보 새로고침하도록 수정

--- frontend/src/containers/SidebarContainer.js
+++ frontend/src/containers/SidebarContainer.js
@@ -2,18 +2,21 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
-import { useRecoilValue } from "recoil";
+import { useRecoilRefresher_UNSTABLE, useRecoilValue } from "recoil";
 import { userAuthInfoSelector } from "../state";
 
 const SidebarContainer = () => {
   const [isLogin, setIsLogin] = useState(false);
   const navigate = useNavigate();
   const userAuthInfo = useRecoilValue(userAuthInfoSelector);
+  const refreshUserAuthInfo = useRecoilRefresher_UNSTABLE(userAuthInfoSelector);
 
   const onLogout = async () => {
-    await axios.get("/auth/logout", { withCredentials: true }).then((res) => {});
-    setIsLogin(false);
-    navigate("/");
+    await axios.get("/auth/logout", { withCredentials: true }).then((res) => {
+      setIsLogin(false);
+      refreshUserAuthInfo();
+      navigate("/");
+    });
   };
 
   return <Sidebar onLogout={onLogout} userAuthInfo={userAuthInfo}></Sidebar>;
